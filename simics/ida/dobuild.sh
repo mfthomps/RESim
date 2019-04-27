94e55650b940ad35c80ca6b462cbc73c4e686e68
Mike Thompson <mfthomps@nps.edu>
2019-04-26 17:04:16 -0700

    remove unused files

--- simics/ida/dobuild.sh
+++
@@ -1,4 +0,0 @@
-make clean
-make
-dpkg-buildpackage -us -uc -d
-sudo dpkg -i ../cgc-monitor-ida_0.1_amd64.deb
--- simics/ida/idaAnalysis.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo starting Ida for $1  directory is $2
-#echo /home/mike/ida-6.4/idaq -B -c -o$2/ida/my.idb $1
-echo /home/mike/ida-6.8/idaq -T"CGC" -B $1
-/home/mike/ida-6.8/idaq -T"CGC" -B $1
