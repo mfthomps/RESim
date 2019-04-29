e38fc1d4a3124e5d294392cd3041440d98355f2a
Mike Thompson <mfthomps@nps.edu>
2019-04-29 08:45:42 -0700

    Sample templates for Simics workspace files

---
+++ simics/workspace/monitor.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#  Define path to the local RESim repo
+#
+export RESIM=/mnt/cgc-monitor/RESim
+
+if [ "$#" -ne 1 ]; then
+    echo "usage: ./monitor.sh <target>"
+    echo "   Where <target> is the base name of a RESim ini file"
+    exit
+fi
+export SIMICS_VER=4.8
+export RESIM_TARGET=$1
+export SIMICS_WORKSPACE=`pwd`
+export SIMICS=/mnt/simics/simics-4.8/simics-4.8.145
+#
+#  For use with driver components
+#
+rm -f driver-ready.flag
+#
+# Launch RESim
+#
+./simics -p $RESIM/simics/monitorCore/launchRESim.py -L $SIMICS_WORKSPACE
