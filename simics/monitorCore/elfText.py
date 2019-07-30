e4bfb7422ef1cb7aeb53db894da113d1da1875fc
Mike Thompson <mfthomps@nps.edu>
2019-07-29 17:15:14 -0700

    use program header instead of sections to find code

--- simics/monitorCore/elfText.py
+++ simics/monitorCore/elfText.py
@@ -14,6 +14,7 @@ class Text():
 def getProgHdr(path):     
     ''' TBD: always use this since kernel does not use section info anyway? '''
     cmd = 'readelf -l %s' % path
+    #grep = 'grep -m1 " LOAD"'
     grep = 'grep " LOAD"'
     proc1 = subprocess.Popen(shlex.split(cmd),stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     proc2 = subprocess.Popen(shlex.split(grep),stdin=proc1.stdout,
@@ -21,17 +22,38 @@ def getProgHdr(path):
 
     proc1.stdout.close() # Allow proc1 to receive a SIGPIPE if proc2 exits.
     out,err=proc2.communicate()
-    print(out)
+    #print(out)
     addr = None
     size = 0
     for line in out.splitlines():
-        parts = out.split()
+        parts = line.split()
+        #print('line was %s num parts is %d' % (line, len(parts)))
         if addr is None:
-            addr = int(parts[3], 16)
-        size = int(parts[4], 16) + size
+            addr = int(parts[2], 16)
+        if len(parts) < 4:
+            print('only %d parts in <%s>, fail' % (len(parts), line))
+            return Text(0,0,0)
+
+        try:
+            size = int(parts[3], 16) + size
+        except:
+            print('bad parse of elf file %s expected int in %s' % (path, line))
+            return Text(0,0,0)
     return Text(addr, 0, size)
 
 def getText(path, lgr):
+    if not os.path.isfile(path):
+        return None
+    retval = None
+    ftype = magic.from_file(path)
+    if 'elf' in ftype.lower():
+        lgr.debug('elfText getText, just use program header')
+        retval = text_segment = getProgHdr(path)
+    else:
+        lgr.debug('elfText getText not elf at %s' % path)
+    return retval
+
+def getTextNOTUSED(path, lgr):
     if not os.path.isfile(path):
         return None
     retval = None
@@ -50,15 +72,16 @@ def getText(path, lgr):
     hack = out[7:]
     #print('readelf got %s from %s' % (hack, path))
     parts = hack.split()
-    if len(parts) < 5:
+    if True or len(parts) < 5:
         ftype = magic.from_file(path)
         if 'elf' in ftype.lower():
             lgr.debug('elfText getText, no sections, use program header')
             retval = text_segment = getProgHdr(path)
+        else:
+            lgr.debug('elfText getText not elf at %s' % path)
     else: 
         addr = int(parts[2], 16)
         offset = int(parts[3], 16)
         size = int(parts[4], 16)
         retval = Text(addr, offset, size)
     return retval
-
