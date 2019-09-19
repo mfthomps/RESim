d390658708d9b4516e19b44e58338a2824d71e8f
Mike Thompson <mfthomps@nps.edu>
2019-09-19 16:38:41 -0700

    handle wrong pid in stop hap

--- simics/monitorCore/hapCleaner.py
+++ simics/monitorCore/hapCleaner.py
@@ -18,12 +18,15 @@ class HapCleaner():
 
 class StopAction():
     ''' hap_clearer is a list of haps to delete
-        and breakpoints to be deleted
+        breakpoints is a list to be deleted
         flist is list of functions to be executed of type stopFunction'''
-    def __init__(self, hap_cleaner, breakpoints, flist=None, break_addrs = []):
+    def __init__(self, hap_cleaner, breakpoints, flist=None, break_addrs = [], pid=None, prelude=None, wrong_pid_action=None):
         self.hap_clean = hap_cleaner
         self.break_addrs = break_addrs
         self.exit_addr = None
+        self.pid = pid
+        self.prelude = prelude
+        self.wrong_pid_action = wrong_pid_action
         if breakpoints is not None:
             self.breakpoints = breakpoints
         else:
@@ -32,11 +35,14 @@ class StopAction():
             self.flist = flist
         else:
             self.flist = []
-    def run(self):
+
+    def run(self, wrong_pid=False):
         ''' Process the functions in the flist '''
         if len(self.flist) > 0:
             fun = self.flist.pop(0)
-            fun.run(self.flist)
+            print('stop action %s wrong pid %r  match %r' % (str(fun.fun), wrong_pid, fun.match_pid))
+            if not (wrong_pid and fun.match_pid):
+                fun.run(self.flist, wrong_pid=wrong_pid)
 
     def getBreaks(self):
         return self.break_addrs
