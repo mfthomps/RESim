2dc41fe98fe3a88a8d16400a0409b8172425cbe9
Mike Thompson <mfthomps@nps.edu>
2019-09-19 16:30:44 -0700

    handling stop haps when stopped in wrong pid, e.g., mode change haps

--- simics/monitorCore/stopFunction.py
+++ simics/monitorCore/stopFunction.py
@@ -16,15 +16,16 @@ def allFuns(flist):
         runFun(f)
 
 class StopFunction():
-    def __init__(self, fun, args, nest=True):
+    def __init__(self, fun, args, nest=True, match_pid=False):
         ''' the function '''
         self.fun = fun
         self.args = args
         ''' nest implies the function should be invoked with the flist as the parameter '''
         self.nest = nest
+        self.match_pid = match_pid
 
         
-    def run(self, flist):
+    def run(self, flist, wrong_pid=False):
         if self.nest:
             self.fun(flist) 
         else:
@@ -32,7 +33,8 @@ class StopFunction():
             ''' args is a list of parameters.  likely a better way to hack this '''
             runFun(self)
             for f in flist:
-                runFun(f)
+                if not (wrong_pid and f.match_pid):
+                    runFun(f)
 
     def getFun(self):
         return self.fun
