d58b03daa1fd51d72b93fdcf5d996a5a25dba087
Mike Thompson <mfthomps@nps.edu>
2019-11-01 15:47:28 -0700

    watchMarks for memcpy type operations

--- simics/monitorCore/dataWatch.py
+++ simics/monitorCore/dataWatch.py
@@ -5,12 +5,14 @@ import hapCleaner
 import decode
 import elfText
 import memUtils
+import watchMarks
 class DataWatch():
     ''' Watch a range of memory and stop when it is read.  Intended for use in tracking
         reads to buffers into which data has been read, e.g., via RECV. '''
     def __init__(self, top, cpu, page_size, context_manager, mem_utils, param, lgr):
         self.start = []
         self.length = []
+        self.cycle = []
         self.read_hap = []
         self.top = top
         self.cpu = cpu
@@ -24,19 +26,7 @@ class DataWatch():
         self.return_break = None
         self.return_hap = None
         self.prev_cycle = None
-        self.watch_marks = []
-
-    class WatchMark():
-        def __init__(self, cycle, ip, msg):
-            self.cycle = cycle
-            self.ip = ip
-            self.msg = msg
-        def getJson(self):
-            retval = {}
-            retval['cycle'] = self.cycle
-            retval['ip'] = self.ip
-            retval['msg'] = self.msg
-            return retval
+        self.watchMarks = watchMarks.WatchMarks(mem_utils, cpu, lgr)
 
     def setRange(self, start, length, msg):
         self.lgr.debug('DataWatch set range start 0x%x length 0x%x' % (start, length))
@@ -52,15 +42,16 @@ class DataWatch():
         if not overlap:
             self.start.append(start)
             self.length.append(length)
-        eip = self.top.getEIP(self.cpu)
-        fixed = unicode(msg, errors='replace')
-        self.watch_marks.append(self.WatchMark(self.cpu.cycles, eip, fixed))
+            self.cycle.append(self.cpu.cycles)
+        if msg is not None:
+            fixed = unicode(msg, errors='replace')
+            self.watchMarks.markCall(fixed)
 
     def close(self, fd):
         ''' called when FD is closed and we might be doing a trackIO '''
         eip = self.top.getEIP(self.cpu)
         msg = 'closed FD: %d' % fd
-        self.watch_marks.append(self.WatchMark(self.cpu.cycles, eip, msg))
+        self.watchMarks.markCall(msg)
         
 
     def watch(self, show_cmp=False, break_simulation=None):
@@ -114,54 +105,97 @@ class DataWatch():
         self.read_hap = []
         if break_simulation is not None: 
             self.break_simulation = break_simulation
+        if self.return_hap is not None:
+            self.context_manager.genDeleteHap(self.return_hap)
+            self.return_hap = None
     
-    def kernelReturnHap(self, pass_this, third, forth, memory):
+    def kernelReturnHap(self, addr, third, forth, memory):
         self.context_manager.genDeleteHap(self.return_hap)
         eax = self.mem_utils.getRegValue(self.cpu, 'syscall_ret')
-        self.lgr.debug('kernelReturnHap, retval 0x%x' % eax)
+        self.lgr.debug('kernelReturnHap, retval 0x%x  addr: 0x%x' % (eax, addr))
+        self.watchMarks.kernel(addr, eax)
         self.watch()
 
-    def kernelReturn(self, dumb):
+    def kernelReturn(self, addr):
         if self.cpu.architecture == 'arm':
             cell = self.top.getCell()
             proc_break = self.context_manager.genBreakpoint(cell, Sim_Break_Linear, Sim_Access_Execute, self.param.arm_ret, 1, 0)
-            self.return_hap = self.context_manager.genHapIndex("Core_Breakpoint_Memop", self.kernelReturnHap, None, proc_break, 'memcpy_return_hap')
+            self.return_hap = self.context_manager.genHapIndex("Core_Breakpoint_Memop", self.kernelReturnHap, addr, proc_break, 'memcpy_return_hap')
         else:
             self.lgr.debug('Only ARM kernel return handled for now') 
             self.watch()
-        
+       
+    class MemSomething():
+        def __init__(self, fun, ret_ip, src, dest, count): 
+            self.fun = fun
+            self.ret_ip = ret_ip
+            self.src = src
+            self.dest = dest
+            self.count = count
       
-    def returnHap(self, pass_this, third, forth, memory):
-        src, dest, ret_ip = pass_this
-        new_src = self.mem_utils.getRegValue(self.cpu, 'r1')
+    def returnHap(self, mem_something, third, forth, memory):
+        if self.return_hap is None:
+            return
         self.context_manager.genDeleteHap(self.return_hap)
-        ''' TBD assumes arm memcpy starts with a LDM     R1!, {R3-R8,R12,LR} '''
-        length = (new_src - src) + 0x20
-        msg = 'copy %d bytes from 0x%x to 0x%x' % (length, src, dest)
-        self.setRange(dest, length, msg) 
-        self.lgr.debug('dataWatch returnHap, return from memcpy src: 0x%x dest: 0x%x new_src: 0x%x length %d ' % (src, dest, new_src, length))
+        self.return_hap = None
+        if mem_something.fun == 'memcpy':
+            ''' TBD assumes arm memcpy starts with a LDM     R1!, {R3-R8,R12,LR} '''
+            if mem_something.count is None:
+                new_src = self.mem_utils.getRegValue(self.cpu, 'r1')
+                mem_something.src = mem_something.src - 0x20
+                count = (new_src - mem_something.src) 
+            self.lgr.debug('dataWatch returnHap, return from %s src: 0x%x dest: 0x%x new_src: 0x%x count %d ' % (mem_something.fun, mem_something.src, 
+                   mem_something.dest, new_src, count))
+            self.setRange(mem_something.dest, count, None) 
+            buf_start = self.findRange(mem_something.src)
+            if buf_start is None:
+                self.lgr.error('dataWatch buf_start for 0x%x is none?' % (mem_something.src))
+            self.watchMarks.copy(mem_something.src, mem_something.dest, count, buf_start)
+        elif mem_something.fun == 'memcmp':
+            buf_start = self.findRange(mem_something.dest)
+            self.watchMarks.compare(mem_something.dest, mem_something.src, mem_something.count, buf_start)
+            self.lgr.debug('dataWatch returnHap, return from %s compare: 0x%x  to: 0x%x count %d ' % (mem_something.fun, mem_something.src, 
+                   mem_something.dest, mem_something.count))
+        else:
+            length = (new_src - mem_something.dest)
+            self.setRange(0, 0, None) 
+            self.lgr.debug('dataWatch returnHap, return from memset dest: 0x%x new_src: 0x%x length %d ' % (mem_something.dest, new_src, length))
+            buf_start = self.findRange(mem_something.dest)
+            self.watchMarks.memset(mem_something.dest, length, buf_start)
         #SIM_break_simulation('return hap')
         #return
         self.watch()
          
-    def handleMemcpy(self, ret_ip):
+    def handleMemStuff(self, mem_stuff):
+        self.lgr.debug('handleMemStuff ret_addr 0x%x fun %s' % (mem_stuff.ret_addr, mem_stuff.fun))
         if self.cpu.architecture == 'arm':
-            dest = self.mem_utils.getRegValue(self.cpu, 'r0')
-            src = self.mem_utils.getRegValue(self.cpu, 'r1')
+            src = None
+            dest = None
+            count = None
+            if mem_stuff.fun == 'memcpy' or mem_stuff.fun == 'memmove': 
+                dest = self.mem_utils.getRegValue(self.cpu, 'r0')
+                src = self.mem_utils.getRegValue(self.cpu, 'r1')
+                #count = self.mem_utils.getRegValue(self.cpu, 'r2')
+            elif mem_stuff.fun == 'memset':
+                dest = self.mem_utils.getRegValue(self.cpu, 'r0')
+            elif mem_stuff.fun == 'memcmp':
+                count = self.mem_utils.getRegValue(self.cpu, 'r4')
+                src = self.mem_utils.getRegValue(self.cpu, 'r12')
+                dest = self.mem_utils.getRegValue(self.cpu, 'r1')
+            mem_stuff
             cell = self.top.getCell()
-            pass_this = [src, dest, ret_ip]
-            proc_break = self.context_manager.genBreakpoint(cell, Sim_Break_Linear, Sim_Access_Execute, ret_ip, 1, 0)
-            self.return_hap = self.context_manager.genHapIndex("Core_Breakpoint_Memop", self.returnHap, pass_this, proc_break, 'memcpy_return_hap')
-            self.lgr.debug('handleMemcpy set hap on ret_ip at 0x%x' % ret_ip)
-            #SIM_break_simulation('wtf is the lr?')
-            
+            mem_something = self.MemSomething(mem_stuff.fun, mem_stuff.ret_addr, src, dest, count)
+            proc_break = self.context_manager.genBreakpoint(cell, Sim_Break_Linear, Sim_Access_Execute, mem_stuff.ret_addr, 1, 0)
+            self.return_hap = self.context_manager.genHapIndex("Core_Breakpoint_Memop", self.returnHap, mem_something, proc_break, 'memcpy_return_hap')
+            self.lgr.debug('handleMemStuff set hap on ret_ip at 0x%x' % mem_stuff.ret_addr)
 
         else:
             self.lgr.debug('Only ARM memcpy handled for now') 
             self.watch()
 
+
     def readHap(self, index, third, forth, memory):
-        #value = SIM_get_mem_op_value_le(memory)
+        ''' watched data has been read (or written) '''
         if self.cpu.cycles == self.prev_cycle:
             return
         self.prev_cycle = self.cpu.cycles
@@ -184,39 +218,39 @@ class DataWatch():
             SIM_run_alone(self.setStopHap, None)
         offset = addr - self.start[index]
         cpl = memUtils.getCPL(self.cpu)
+        ''' If execution outside of text segment, check for mem-something library call '''
         start, end = self.context_manager.getText()
         call_sp = None
-        if eip > end and cpl != 0:
-            ''' from so library, check for cpy functions '''
+        self.lgr.debug('readHap eip 0x%x start 0x%x  end 0x%x' % (eip, start, end))
+        if cpl != 0:
             if not self.break_simulation:
                 ''' prevent stack trace from triggering haps '''
                 self.stopWatch()
             st = self.top.getStackTraceQuiet()
-            self.lgr.debug('%s' % st.getJson()) 
+            #self.lgr.debug('%s' % st.getJson()) 
             ''' look for memcpy'ish... TBD generalize '''
-            ret_ip = st.memcpy()
-            if ret_ip is not None:
-                self.lgr.debug('DataWatch readHap ret_ip 0x%x' % (ret_ip))
-                SIM_run_alone(self.handleMemcpy, ret_ip)
+            mem_stuff = st.memsomething()
+            if mem_stuff is not None:
+                self.lgr.debug('DataWatch readHap ret_ip 0x%x' % (mem_stuff.ret_addr))
+                SIM_run_alone(self.handleMemStuff, mem_stuff)
             else:
-                self.lgr.debug('DataWatch readHap not memcpy, reset the watch')
+                self.lgr.debug('DataWatch readHap not memsomething, reset the watch')
                 self.watch()
         if op_type == Sim_Trans_Load:
-
             self.lgr.debug('Data read from 0x%x within input buffer (offset of %d into buffer of %d bytes starting at 0x%x) eip: 0x%x cycle:0x%x' % (addr, 
                     offset, self.length[index], self.start[index], eip, self.cpu.cycles))
             msg = ('Data read from 0x%x within input buffer (offset of %d into %d bytes starting at 0x%x) eip: 0x%x' % (addr, 
                         offset, self.length[index], self.start[index], eip))
             self.context_manager.setIdaMessage(msg)
-            mark_msg = 'Read from 0x%08x offset %4d into 0x%8x (buf size %4d) %s' % (addr, offset, self.start[index], self.length[index], self.getCmp())
-            self.watch_marks.append(self.WatchMark(self.cpu.cycles, eip, mark_msg))
+            #mark_msg = 'Read from 0x%08x offset %4d into 0x%8x (buf size %4d) %s' % (addr, offset, self.start[index], self.length[index], self.getCmp())
+            self.watchMarks.dataRead(addr, self.start[index], self.length[index], self.getCmp())
             if self.break_simulation:
                 SIM_break_simulation('DataWatch read data')
 
             if cpl == 0:
                 if not self.break_simulation:
                     self.stopWatch()
-                SIM_run_alone(self.kernelReturn, None)
+                SIM_run_alone(self.kernelReturn, addr)
             elif call_sp is None:
                 ''' not kernel and not library copy. look for compare '''
                 for i in range(3):
@@ -282,18 +316,42 @@ class DataWatch():
         for index in range(len(self.start)):
             if self.start[index] != 0:
                 end = self.start[index] + self.length[index]
+                self.lgr.debug('findRange is 0x%x between 0x%x and 0x%x?' % (addr, self.start[index], end))
                 if addr >= self.start[index] and addr <= end:
                     return self.start[index]
         return None
 
     def getWatchMarks(self):
-        retval = []
-        for mark in self.watch_marks:
-            retval.append(mark.getJson())
-        return retval        
+        return self.watchMarks.getWatchMarks()
 
     def goToMark(self, index):
-        mark = self.watch_marks[index]
-        SIM_run_command('pselect %s' % self.cpu.name)
-        SIM_run_command('skip-to cycle=%d' % mark.cycle)
- 
+        cycle = self.watchMarks.getCycle(index)
+        if cycle is not None:
+            SIM_run_command('pselect %s' % self.cpu.name)
+            SIM_run_command('skip-to cycle=%d' % cycle)
+        else:
+           self.lgr.error('No data mark with index %d' % index)
+
+    def clearWatchMarks(self): 
+        self.watchMarks.clearWatchMarks()
+
+    def clearWatches(self, cycle=None):
+        self.lgr.debug('DataWatch clear Watches')
+        self.stopWatch()
+        if cycle is None:
+            del self.start[:]
+            del self.length[:]
+            del self.cycle[:]
+        else:
+            found = None
+            for index in range(len(self.cycle)):
+                self.lgr.debug('clearWAtches cycle 0x%x list 0x%x' % (cycle, self.cycle[index]))
+                if self.cycle[index] > cycle:
+                    self.lgr.debug('clearWatches found cycle index %s' % index)
+                    found = index
+                    break
+            if found is not None:
+                self.start = self.start[:index]
+                self.length = self.length[:index]
+                self.cycle = self.cycle[:index]
+                
---
+++ simics/monitorCore/watchMarks.py
@@ -0,0 +1,167 @@
+class WatchMarks():
+    def __init__(self, mem_utils, cpu, lgr):
+        self.mark_list = []
+        self.mem_utils = mem_utils
+        self.cpu = cpu
+        self.lgr = lgr
+        self.prev_ip = None
+
+    class CallMark():
+        def __init__(self, msg):
+            self.msg = msg
+        def getMsg(self):
+            return self.msg
+
+    class CopyMark():
+        def __init__(self, src, dest, length, buf_start):
+            self.src = src
+            self.dest = dest
+            self.length = length
+            self.buf_start = buf_start
+            offset = src - buf_start
+            self.msg = 'Copy %d bytes from 0x%x to 0x%x. (from offset %d into buffer at 0x%x)' % (length, src, dest, offset, buf_start)
+        def getMsg(self):
+            return self.msg
+
+    class SetMark():
+        def __init__(self, dest, length, buf_start):
+            self.dest = dest
+            self.length = length
+            self.buf_start = buf_start
+            offset = dest - buf_start
+            self.msg = 'memset %d bytes starting 0x%x (offset %d into buffer at 0x%x)' % (length, dest, offset, buf_start)
+        def getMsg(self):
+            return self.msg
+
+    class DataMark():
+        def __init__(self, addr, start, length, cmp_ins):
+            self.addr = addr
+            self.offset = addr - start
+            self.start = start
+            self.length = length
+            self.cmp_ins = cmp_ins
+            self.end_addr = None
+
+        def getMsg(self):
+            if self.end_addr is None:
+                mark_msg = 'Read from 0x%08x offset %4d into 0x%8x (buf size %4d) %s' % (self.addr, self.offset, self.start, self.length, self.cmp_ins)
+            else:
+                length = self.end_addr- self.addr
+                mark_msg = 'Iterate over 0x%08x-0x%08x (%d bytes) starting offset %4d into 0x%8x (buf size %4d) %s' % (self.addr, 
+                     self.end_addr, length, self.offset, self.start, self.length, self.cmp_ins)
+            return mark_msg
+
+        def addrRange(self, addr):
+            self.end_addr = addr
+
+    class KernelMark():
+        def __init__(self, addr, count):
+            self.addr = addr
+            self.count = count
+            self.msg = 'Kernel read %d bytes from 0x%x' % (count, addr)
+        def getMsg(self):
+            return self.msg
+
+    class CompareMark():
+        def __init__(self, ours, theirs, count, the_str, buf_start):
+            self.the_str = the_str
+            self.ours = ours    
+            self.theirs = theirs    
+            self.count = count    
+            offset = ours - buf_start
+            self.msg = 'memcmp 0x%x (%d bytes into buffer at 0x%x) to %s (at 0x%x, %d bytes)' % (ours, offset, buf_start, self.the_str, theirs, count)
+        def getMsg(self):
+            return self.msg
+
+    class WatchMark():
+        def __init__(self, cycle, ip, msg):
+            self.cycle = cycle
+            self.ip = ip
+            self.msg = msg
+        def getJson(self):
+            retval = {}
+            retval['cycle'] = self.cycle
+            retval['ip'] = self.ip
+            retval['msg'] = self.msg.getMsg()
+            return retval
+
+    def markCall(self, msg):
+        ip = self.mem_utils.getRegValue(self.cpu, 'pc')
+        cm = self.CallMark(msg)
+        self.mark_list.append(self.WatchMark(self.cpu.cycles, ip, cm))
+        self.lgr.debug('watchMarks markCall 0x%x %s' % (ip, msg))
+        self.prev_ip = ip
+   
+    def dataRead(self, addr, start, length, cmp_ins): 
+        ip = self.mem_utils.getRegValue(self.cpu, 'pc')
+        ''' TBD generalize for loops that make multiple refs? '''
+        if ip != self.prev_ip:
+            dm = self.DataMark(addr, start, length, cmp_ins)
+            self.mark_list.append(self.WatchMark(self.cpu.cycles, ip, dm))
+            self.lgr.debug('watchMarks dataRead 0x%x %s' % (ip, dm.getMsg()))
+        else:
+            if self.prev_ip is not None:
+                pm = self.mark_list[-1]
+                self.lgr.debug('pm class is %s' % pm.msg.__class__.__name__)
+                if isinstance(pm.msg, self.DataMark):
+                    pm.msg.addrRange(addr)
+                    self.lgr.debug('watchMarks dataRead 0x%x range 0x%x' % (ip, addr))
+                else:
+                    dm = self.DataMark(addr, start, length, cmp_ins)
+                    self.mark_list.append(self.WatchMark(self.cpu.cycles, ip, dm))
+                    self.lgr.debug('watchMarks dataRead followed markCall 0x%x %s' % (ip, dm.getMsg()))
+        self.prev_ip = ip
+
+    def getWatchMarks(self):
+        retval = []
+        for mark in self.mark_list:
+            retval.append(mark.getJson())
+        return retval        
+
+    def getCycle(self, index):
+        self.lgr.debug('watchMarks getCycle index %d len %s' % (index, len(self.mark_list)))
+        if index < len(self.mark_list):
+            return self.mark_list[index].cycle
+        else:
+            return None
+
+    def removeRedundantDataMark(self, dest):
+        if self.prev_ip is not None:
+            pm = self.mark_list[-1]
+            if isinstance(pm.msg, self.DataMark):
+                if pm.msg.addr == dest:
+                    ''' a copy record for the same data read previously recorded, remove the redundant data read '''
+                    del self.mark_list[-1]
+
+    def copy(self, src, dest, length, buf_start):
+        ip = self.mem_utils.getRegValue(self.cpu, 'pc')
+        cm = self.CopyMark(src, dest, length, buf_start)
+        self.lgr.debug('watchMarks copy 0x%x %s' % (ip, cm.getMsg()))
+        self.removeRedundantDataMark(dest)
+        self.mark_list.append(self.WatchMark(self.cpu.cycles, ip, cm))
+        
+
+    def memset(self, dest, length, buf_start):
+        ip = self.mem_utils.getRegValue(self.cpu, 'pc')
+        sm = self.SetMark(dest, length, buf_start)
+        self.mark_list.append(self.WatchMark(self.cpu.cycles, ip, sm))
+        self.lgr.debug('watchMarks memset 0x%x %s' % (ip, sm.getMsg()))
+
+    def kernel(self, addr, count):
+        ip = self.mem_utils.getRegValue(self.cpu, 'pc')
+        km = self.KernelMark(addr, count)
+        self.mark_list.append(self.WatchMark(self.cpu.cycles, ip, km))
+        self.lgr.debug('watchMarks kernel 0x%x %s' % (ip, km.getMsg()))
+
+    def compare(self, ours, theirs, count, buf_start):
+        ip = self.mem_utils.getRegValue(self.cpu, 'pc')
+        the_str = self.mem_utils.readString(self.cpu, theirs, count)
+        cm = self.CompareMark(ours, theirs, count, the_str, buf_start) 
+        self.removeRedundantDataMark(ours)
+        self.mark_list.append(self.WatchMark(self.cpu.cycles, ip, cm))
+        self.lgr.debug('watchMarks compare 0x%x %s' % (ip, cm.getMsg()))
+
+    def clearWatchMarks(self): 
+        del self.mark_list[:] 
+        self.prev_ip = None
+
