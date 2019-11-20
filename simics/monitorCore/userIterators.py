442fbbaa9e93e20e338a6c49b5f7ee54462ec8f5
Mike Thompson <mfthomps@nps.edu>
2019-11-20 09:18:00 -0800

    allow analyst to identify function as an iterator

---
+++ simics/monitorCore/userIterators.py
@@ -0,0 +1,26 @@
+import os
+class UserIterators():
+    def __init__(self, path):
+        self.path = path
+        self.iterators = []
+        if os.path.isfile(path):
+            with open(path) as fh:
+                for line in fh:
+                    try:
+                       fun = int(line.strip(), 16)
+                    except ValueError:
+                       print('Failed to read function addresses from %s' % path)
+                       return
+                    self.iterators.append(fun)
+
+    def add(self, fun):
+        if fun not in self.iterators and fun is not None:
+            self.iterators.append(fun) 
+            with open(self.path, 'w') as fh:
+                for fun in self.iterators:
+                    fh.write('0x%x\n' % fun)
+
+    def isIterator(self, fun):
+        if fun in self.iterators:
+            return True
+        return False
