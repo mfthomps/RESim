4e9be450f9d457e66691959f69c04f98a6c5d375
Mike Thompson <mfthomps@nps.edu>
2019-11-22 17:45:54 -0800

    IDA 7 changes

--- simics/ida/dumpFuns.py
+++ simics/ida/dumpFuns.py
@@ -8,10 +8,14 @@ start = get_segm_start(ea)
 end = get_segm_end(ea)
 for function_ea in Functions(start,  end):
     funs[function_ea] = {}
-    end = get_func_attr(function_ea, 4)
-    funs[function_ea]['start'] = function_ea
-    funs[function_ea]['end'] = end
-    funs[function_ea]['name'] = get_func_name(function_ea)
+    try:
+        end = get_func_attr(function_ea, FUNCATTR_END)
+        funs[function_ea]['start'] = function_ea
+        funs[function_ea]['end'] = end
+        funs[function_ea]['name'] = get_func_name(function_ea)
+    except KeyError:
+        print('failed getting attribute for 0x%x' % function_ea)
+        pass
 
 with open(fname+'.funs', "w") as fh:
     json.dump(funs, fh)
