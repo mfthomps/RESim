84be574b35015a8536776a58d6c20ba55ef92862
Mike Thompson <mfthomps@nps.edu>
2019-12-20 17:43:17 -0800

    add nx info to returnded data, more work needed

---
+++ simics/ida/findLdm.py
@@ -0,0 +1,35 @@
+'''
+Find arm ldm of r0, r1, r2 and pc 
+'''
+def testBit(int_value, bit):
+    mask = 1 << bit
+    return(int_value & mask)
+
+def bitRange(value, start, end):
+    shifted = value >> start
+    num_bits = (end - start) + 1
+    mask = 2**num_bits - 1
+    retval = shifted & mask
+    return retval
+
+ea = get_screen_ea()
+print 'ea is %x' % ea
+start = get_segm_start(ea)
+end = get_segm_end(ea)
+print('code at 0x%x - 0x%x' % (start, end))
+for addr in range(start, end, 4):
+    ins = idc.read_dbg_dword(addr)
+    if ins is None:
+        print('addr 0x%x reads none')
+    opcode = bitRange(ins,25,27)
+    if opcode == 4 and testBit(ins, 20):
+        base_reg = bitRange(ins, 16, 19)
+        if base_reg == 13:
+            reg_list = bitRange(ins, 0, 15)
+            if testBit(reg_list, 0) and testBit(reg_list, 15):
+                print('loaded r0 and PC from stack at 0x%x' % addr)
+            if testBit(reg_list, 1) and testBit(reg_list, 15):
+                print('loaded r1 and PC from stack at 0x%x' % addr)
+            if testBit(reg_list, 2) and testBit(reg_list, 15):
+                print('loaded r2 and PC from stack at 0x%x' % addr)
+            
---
+++ simics/ida/liftSP.py
@@ -0,0 +1,41 @@
+'''
+Find arm add sp, ... instructions
+'''
+import sys
+def testBit(int_value, bit):
+    mask = 1 << bit
+    return(int_value & mask)
+
+def bitRange(value, start, end):
+    shifted = value >> start
+    num_bits = (end - start) + 1
+    mask = 2**num_bits - 1
+    retval = shifted & mask
+    return retval
+
+def rotateRight(val, r_bits):
+    ror = lambda val, r_bits, max_bits: \
+        ((val & (2**max_bits-1)) >> r_bits%max_bits) | \
+        (val << (max_bits-(r_bits%max_bits)) & (2**max_bits-1))
+    return ror(val, r_bits, 32)
+
+def findLift(minimum=0):
+    ea = get_screen_ea()
+    print 'ea is %x' % ea
+    start = get_segm_start(ea)
+    end = get_segm_end(ea)
+    print('code at 0x%x - 0x%x' % (start, end))
+    for addr in range(start, end, 4):
+        ins = idc.read_dbg_dword(addr)
+        opcode = bitRange(ins, 21,24)
+        if opcode == 4:
+            rd = bitRange(ins, 12, 15)
+            if rd == 13:
+                imm = bitRange(ins, 0, 7)
+                rotate = bitRange(ins, 8, 11)
+                op2 = rotateRight(imm, rotate*2)
+                if op2 >= minimum:
+                    rn = bitRange(ins, 16, 19)
+                    print('add 0x%x  (im 0x%x shift %d) to %d into sp at addr 0x%x' % (op2, imm, rotate, rn, addr))
+           
+findLift() 
