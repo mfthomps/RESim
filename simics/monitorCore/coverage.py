e4b60d0c5b316792de7785669ea96e47931aa454
Mike Thompson <mfthomps@nps.edu>
2020-05-04 15:48:41 -0700

    add diff and stop...

--- simics/monitorCore/coverage.py
+++ simics/monitorCore/coverage.py
@@ -22,11 +22,25 @@ class Coverage():
         else:
             self.lgr.debug('Coverage, no blocks at %s' % block_file)
 
+    def stopCover(self):
+        self.lgr.debug('coverage, stopCover')
+        for bp in self.bp_list:
+            try:
+                SIM_delete_breakpoint(bp)
+            except:
+                self.lgr.debug('coverage, stopCover bp %d does not exist?' % bp)
+        self.bp_list = []
+        if self.bb_hap is not None:
+            SIM_hap_delete_callback_id('Core_Breakpoint_Memop', self.bb_hap)
+            self.bb_hap = None
+            self.funs_hit = []
+            self.blocks_hit = []
 
     def cover(self):
         if self.blocks is None:
             self.lgr.error('No basic blocks defined')
             return
+        self.stopCover()
         resim_context = self.context_manager.getResimContext()
         for fun in self.blocks:
             for bb in self.blocks[fun]['blocks']:
@@ -38,8 +52,9 @@ class Coverage():
         #self.bb_hap = self.context_manager.genHapRange("Core_Breakpoint_Memop", self.bbHap, None, self.bp_list[0], self.bp_list[-1], name='coverage_hap')
         self.bb_hap = SIM_hap_add_callback_range("Core_Breakpoint_Memop", self.bbHap, None, self.bp_list[0], self.bp_list[-1])
 
-    def bbHap(self, dumb, third, forth, memory):
-        if self.context_manager.watchingThis():
+    def bbHap(self, dumb, third, break_num, memory):
+        ''' HAP when a bb is hit '''
+        if self.context_manager.watchingThis() and self.bb_hap is not None:
             addr = memory.logical_address
             if addr not in self.blocks_hit:
                 self.blocks_hit.append(addr)
@@ -48,24 +63,58 @@ class Coverage():
                     self.funs_hit.append(addr)
                 self.lgr.debug('bbHap hit 0x%x %s count %d of %d   Functions %d of %d' % (addr, addr_str, 
                        len(self.blocks_hit), self.block_total, len(self.funs_hit), len(self.blocks)))
+            ''' remove from list '''
+            if break_num in self.bp_list:
+                self.bp_list.remove(break_num)
+
+    def saveHits(self, fname):
+        ''' save blocks_hit to named file '''
+        save_name = '%s.%s.hits' % (self.full_path, fname)
+        with open(save_name, 'w') as outj:
+            json.dump(self.blocks_hit, outj)
 
     def showCoverage(self):
+        ''' blocks_hit and funs_hit are populated via the HAP. '''
         cover = (len(self.blocks_hit)*100) / self.block_total 
         print('Hit %d of %d blocks  (%d percent)' % (len(self.blocks_hit), self.block_total, cover))
         print('Hit %d of %d functions' % (len(self.funs_hit), len(self.blocks)))
+
+    def saveCoverage(self, fname = None):
+        self.lgr.debug('saveCoverage for %d functions' % len(self.funs_hit))
+        ''' New dictionary for json.   Key is function address as a string '''
         hit_blocks = {}
+        ''' funs_hit is list of addresses as integers '''
         for fun in self.funs_hit:
-            fun_str = '%s' % fun
+            fun_str = '%d' % fun
             hit_blocks[fun_str] = []
+
+        ''' Create a list of bb hit per function '''
         for bb in self.blocks_hit:
+            ''' Find the function that contains the bb '''
             for ofun in self.blocks:
                 if bb in self.blocks[ofun]['blocks']:
+                   ''' bb is in ofun '''
                    try:
                        hit_blocks[ofun].append(bb)       
+                       break
                    except:
-                       self.lgr.error('%s not found in hit blocks???' % ofun)
-                   break
+                       self.lgr.debug('%s not found in hit blocks, meaning not in funs_hit???  bb was 0x%x, likely IDA error' % (ofun, bb))
         s = json.dumps(hit_blocks)
-        with open(self.full_path+'.hits', 'w') as fh:
+        if fname is None:
+            save_name = '%s.hits' % self.full_path
+        else:
+            save_name = '%s.%s.hits' % (self.full_path, fname)
+        with open(save_name, 'w') as fh:
             fh.write(s)
-  
+ 
+    def difCoverage(self, fname):
+        save_name = '%s.%s.hits' % (self.full_path, fname)
+        if not os.path.isfile(save_name):
+            print('No file named %s' % save_name)
+            return
+        with open(save_name) as fh:
+            jhits = json.load(fh)
+            for i in range(len(jhits)):
+                if jhits[i] != self.blocks_hit[i]:
+                    print('new 0x%x  old 0x%x index: %d' % (self.blocks_hit[i], jhits[i], i))
+                    break
