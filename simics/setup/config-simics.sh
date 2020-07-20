5824fa0cc867efb4d40df35ea9f96870ebca2e9c
Mike Thompson <mfthomps@nps.edu>
2020-07-20 10:43:40 -0700

    fix to lmgrd

--- simics/setup/config-simics.sh
+++ simics/setup/config-simics.sh
@@ -3,12 +3,11 @@
 # Configure Simics license for a server.  Run this after configuring the server
 # for RESim and configuring a user for RESim.
 #
-myMAC=`/sbin/ifconfig | grep eth0 | awk '{print toupper($5);}' | sed s/://g`
-myFile=$myMAC.lic
-licensepath=/mnt/simics/simics-4.8/simics-4.8.75/licenses/$myFile
-echo "Close Simics after it starts"
+cp lmgrdFix /usr/bin/
+cp lmgrdFixService /etc/init.d/
+sudo update-rd.d lmgrdFixService defaults
+sudo service lmgrdFixService start
 cd ~/workspace
-./simics-gui -license-file $licensepath
 sudo bin/vmp-kernel-install
 sudo /mnt/simics/simics-4.8/simics-4.8.170/scripts/../vmxmon/scripts/install
 
