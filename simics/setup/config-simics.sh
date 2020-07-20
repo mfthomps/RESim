ab470610a0512ee748dd97e2b91165d2c23af75d
Mike Thompson <mfthomps@nps.edu>
2020-07-20 10:49:29 -0700

    fix typos... debug...

--- simics/setup/config-simics.sh
+++ simics/setup/config-simics.sh
@@ -3,9 +3,9 @@
 # Configure Simics license for a server.  Run this after configuring the server
 # for RESim and configuring a user for RESim.
 #
-cp lmgrdFix /usr/bin/
-cp lmgrdFixService /etc/init.d/
-sudo update-rd.d lmgrdFixService defaults
+sudo cp lmgrdFix /usr/bin/
+sudo cp lmgrdFixService /etc/init.d/
+sudo update-rc.d lmgrdFixService defaults
 sudo service lmgrdFixService start
 cd ~/workspace
 sudo bin/vmp-kernel-install
