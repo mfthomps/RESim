3754cba503bdc3c4e4fa135cc99c26d2d5bad6f9
Mike Thompson <mfthomps@nps.edu>
2020-12-22 07:46:50 -0800

    remove debug

--- simics/monitorCore/traceMalloc.py
+++ simics/monitorCore/traceMalloc.py
@@ -48,10 +48,10 @@ class TraceMalloc():
     def mallocHap(self, dumb, context, break_num, memory):
         if self.malloc_hap is not None:
             cpu, comm, pid = self.task_utils.curProc() 
-            self.lgr.debug('TraceMalloc mallocHap pid:%d' % pid)
+            #self.lgr.debug('TraceMalloc mallocHap pid:%d' % pid)
             if cpu.architecture == 'arm':
                 size = self.mem_utils.getRegValue(self.cpu, 'r0') 
-                self.lgr.debug('malloc size %d' % size)
+                #self.lgr.debug('malloc size %d' % size)
                 ret_addr = self.mem_utils.getRegValue(self.cpu, 'lr') 
             else:
                 sp = self.mem_utils.getRegValue(self.cpu, 'sp')
@@ -64,10 +64,10 @@ class TraceMalloc():
     def freeHap(self, dumb, context, break_num, memory):
         if self.free_hap is not None:
             cpu, comm, pid = self.task_utils.curProc() 
-            self.lgr.debug('TraceMalloc freeHap pid:%d' % pid)
+            #self.lgr.debug('TraceMalloc freeHap pid:%d' % pid)
             if cpu.architecture == 'arm':
                 addr = self.mem_utils.getRegValue(self.cpu, 'r0') 
-                self.lgr.debug('free addr 0x%x' % addr)
+                #self.lgr.debug('free addr 0x%x' % addr)
             else:
                 sp = self.mem_utils.getRegValue(self.cpu, 'sp')
                 addr = self.mem_utils.readPtr(self.cpu, sp+self.mem_utils.WORD_SIZE)
@@ -76,13 +76,13 @@ class TraceMalloc():
     def mallocEndHap(self, malloc_rec, context, break_num, memory):
         if self.malloc_hap_ret is not None:
             cpu, comm, pid = self.task_utils.curProc() 
-            self.lgr.debug('TraceMalloc mallocEndHap pid:%d' % pid)
+            #self.lgr.debug('TraceMalloc mallocEndHap pid:%d' % pid)
             if cpu.architecture == 'arm':
                 addr = self.mem_utils.getRegValue(self.cpu, 'r0') 
-                self.lgr.debug('malloc addr 0x%x' % addr)
+                #self.lgr.debug('malloc addr 0x%x' % addr)
             else:
                 addr = self.mem_utils.getRegValue(self.cpu, 'eax') 
-                self.lgr.debug('malloc addr 0x%x' % addr)
+                #self.lgr.debug('malloc addr 0x%x' % addr)
             malloc_rec.addr = addr
             self.malloc_list.append(malloc_rec)
             self.context_manager.genDeleteHap(self.malloc_hap_ret)
