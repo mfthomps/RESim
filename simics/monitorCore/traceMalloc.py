43624d13bcf905990c9a656f018803f68fe272ce
Mike Thompson <mfthomps@nps.edu>
2021-01-05 15:29:42 -0800

    remove debug

--- simics/monitorCore/traceMalloc.py
+++ simics/monitorCore/traceMalloc.py
@@ -48,7 +48,7 @@ class TraceMalloc():
     def mallocHap(self, dumb, context, break_num, memory):
         if self.malloc_hap is not None:
             cpu, comm, pid = self.task_utils.curProc() 
-            self.lgr.debug('TraceMalloc mallocHap pid:%d' % pid)
+            #self.lgr.debug('TraceMalloc mallocHap pid:%d' % pid)
             if cpu.architecture == 'arm':
                 size = self.mem_utils.getRegValue(self.cpu, 'r0') 
                 #self.lgr.debug('malloc size %d' % size)
@@ -72,19 +72,19 @@ class TraceMalloc():
             else:
                 sp = self.mem_utils.getRegValue(self.cpu, 'sp')
                 addr = self.mem_utils.readPtr(self.cpu, sp+self.mem_utils.WORD_SIZE)
-                self.lgr.debug('free addr 0x%x' % addr)
+                #self.lgr.debug('free addr 0x%x' % addr)
             self.dataWatch.recordFree(addr)
 
     def mallocEndHap(self, malloc_rec, context, break_num, memory):
         if self.malloc_hap_ret is not None:
             cpu, comm, pid = self.task_utils.curProc() 
-            self.lgr.debug('TraceMalloc mallocEndHap pid:%d' % pid)
+            #self.lgr.debug('TraceMalloc mallocEndHap pid:%d' % pid)
             if cpu.architecture == 'arm':
                 addr = self.mem_utils.getRegValue(self.cpu, 'r0') 
                 #self.lgr.debug('malloc addr 0x%x' % addr)
             else:
                 addr = self.mem_utils.getRegValue(self.cpu, 'eax') 
-                self.lgr.debug('malloc addr 0x%x, size: %d' % (addr, malloc_rec.size))
+                #self.lgr.debug('malloc addr 0x%x, size: %d' % (addr, malloc_rec.size))
             malloc_rec.addr = addr
             self.malloc_list.append(malloc_rec)
             self.context_manager.genDeleteHap(self.malloc_hap_ret)
