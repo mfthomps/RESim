a4e9fdce6e644aa49fa18c2e18ae604fe47dc8fc
Mike Thompson <mfthomps@nps.edu>
2021-08-11 02:41:49 -0700

    support simics 4

--- simics/testing/cadet-test/findWindow.sh
+++ simics/testing/cadet-test/findWindow.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 mywin=$(xdotool search --name "$1")
-xdotool windowactivate $mywin
+first=$( echo $mywin | head -n1  | awk '{print $1;}' )
+echo "first is $first"
+xdotool windowactivate $first
