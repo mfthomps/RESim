66789942277da7ea609f360cf5f05d3bd6fe6bce
Mike Thompson <mfthomps@nps.edu>
2021-09-23 10:25:30 -0700

    handle afl naming here

---
+++ simics/monitorCore/aflPath.py
@@ -0,0 +1,33 @@
+import os
+import glob
+def getAFL_OUTPUT():
+    afl_dir = os.getenv('AFL_OUTPUT')
+    if afl_dir is None:
+        afl_dir = os.path.join(os.getenv('AFL_DATA'), 'output')
+    if afl_dir is None:
+        print('No AFL_DATA or AFL_OUTPUT')
+    return afl_dir
+
+def getAFLPath(target, index, instance):
+    retval = None
+    afl_dir = getAFL_OUTPUT()
+    if instance is None:
+        glob_mask = '%s/%s/queue/id:*0%s,src*' % (afl_dir, target, index)
+        glist = glob.glob(glob_mask)
+        if len(glist) == 0:
+            glob_mask = '%s/%s/queue/id:*0%s,sync*' % (afl_dir, target, index)
+            glist = glob.glob(glob_mask)
+    else:
+        resim_instance = 'resim_%d' % instance
+        glob_mask = '%s/%s/%s/queue/id:*0%s,src*' % (afl_dir, target, resim_instance, index)
+        glist = glob.glob(glob_mask)
+        if len(glist) == 0:
+            glob_mask = '%s/%s/%s/queue/id:*0%s,sync*' % (afl_dir, target, resim_instance, index)
+            glist = glob.glob(glob_mask)
+    if len(glist) == 0:
+        print('No files found looking for %s' % glob_mask)
+    elif len(glist) == 1:
+        retval = glist[0]
+    else:
+        print('Too many matches, try adding leading zeros?')
+    return retval
