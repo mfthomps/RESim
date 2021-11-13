483d87fa48a678309cbc3f3cf6017d7c2f9c9a41
Mike Thompson <mfthomps@nps.edu>
2021-11-12 16:39:31 -0800

    support skipping diffs and blank lines

--- simics/bin/traceDiff.py
+++ simics/bin/traceDiff.py
@@ -11,6 +11,9 @@ class getter():
 
     def getVP(self, line):
         parts = line[16:].split()
+        if len(parts) < 5:
+            print('failed in getVP for line %s' % line)
+            exit(1)
         v = parts[3]
         p = parts[4]
         return v, p
@@ -18,7 +21,11 @@ class getter():
     def getCPU(self, line):
         parts = line[17:].split()
         try:
-            return parts[0].strip()
+            stuff = parts[0].strip()
+            if len(stuff) > 0:
+                return stuff
+            else:
+                return None
         except:
             #print('could not get cpu from line %s' % line)
             #print('tried parts from %s' % line[17:])
@@ -36,6 +43,9 @@ class getter():
             while not ok:
                 v, p = self.getVP(line)
                 next_line = self.lines[self.index+1]
+                if len(next_line.strip()) == 0:
+                    self.index = self.index+1
+                    continue
                 vn, pn = self.getVP(next_line)
                 if next_line.startswith('data') and v == vn and p != pn:
                     #print('skipping %s' % line)
@@ -53,6 +63,7 @@ def main():
     parser = argparse.ArgumentParser(prog='diffTrace', description='Show differences in 2 instruction traces')
     parser.add_argument('trace1', action='store', help='The first trace file.')
     parser.add_argument('trace2', action='store', help='The second trace file.')
+    parser.add_argument('-i', '--ignore', action='store', type=int, default=0, help='Number of differences to ignore.')
     args = parser.parse_args()
 
     get1 = getter(args.trace1)
@@ -68,15 +79,20 @@ def main():
     
     rest1 = line1[16:]
     rest2 = line2[16:]
-    while rest1 == rest2:
-        #print('get1')
-        line1 = get1.nextLine()
-        #print('\tline1 %s' % line1)
-        #print('get2')
-        line2 = get2.nextLine()
-        #print('\tline2 %s' % line2)
-        rest1 = line1[16:]
-        rest2 = line2[16:]
+    diffs = 0
+    while diffs <= args.ignore: 
+        while rest1 == rest2:
+            #print('get1')
+            line1 = get1.nextLine()
+            #print('\tline1 %s' % line1)
+            #print('get2')
+            line2 = get2.nextLine()
+            #print('\tline2 %s' % line2)
+            rest1 = line1[16:]
+            rest2 = line2[16:]
+        diffs += 1
+        rest1 = 'ok'
+        rest2 = 'ok'
     print('line1 line number %d %s' % (get1.getIndex(), line1))
     print('line2  line number %d %s' % (get2.getIndex(), line2))
 
