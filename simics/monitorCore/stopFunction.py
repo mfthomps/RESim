2c85ccfadfedbdd33934a34ac006b320e373455a
Mike Thompson <mfthomps@nps.edu>
2021-11-30 15:50:50 -0800

    optional parameters for hap cleaners (function chains)

--- simics/monitorCore/stopFunction.py
+++ simics/monitorCore/stopFunction.py
@@ -1,9 +1,12 @@
 '''
 Element in a chain of functions to be invoked from a stop hap.
 '''
-def runFun(f):
+def runFun(f, cb_param=None):
         if len(f.args) == 0:
             f.fun()
+        elif len(f.args) == 1 and f.args[0]=='cb_param':
+            ''' the function handling the stop hap has provided the parameter rather than the flist creator '''
+            f.fun(cb_param)
         elif len(f.args) == 1:
             f.fun(f.args[0])
         elif len(f.args) == 2:
@@ -25,13 +28,13 @@ class StopFunction():
         self.match_pid = match_pid
 
         
-    def run(self, flist, wrong_pid=False):
+    def run(self, flist, wrong_pid=False, cb_param=None):
         if self.nest:
             self.fun(flist) 
         else:
             ''' TBD assume remaining functions are not hap-related'''
             ''' args is a list of parameters.  likely a better way to hack this '''
-            runFun(self)
+            runFun(self, cb_param=cb_param)
             for f in flist:
                 if not (wrong_pid and f.match_pid):
                     runFun(f)
