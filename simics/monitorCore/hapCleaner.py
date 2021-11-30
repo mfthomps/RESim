e28348950e085e3066174b83ed6bf664be9089e9
Mike Thompson <mfthomps@nps.edu>
2021-11-30 15:48:46 -0800

    optional callback parameter

--- simics/monitorCore/hapCleaner.py
+++ simics/monitorCore/hapCleaner.py
@@ -36,13 +36,13 @@ class StopAction():
         else:
             self.flist = []
 
-    def run(self, wrong_pid=False):
-        ''' Process the functions in the flist '''
+    def run(self, wrong_pid=False, cb_param=None):
+        ''' Process the functions in the flist, these are the stopFunction class'''
         if len(self.flist) > 0:
             fun = self.flist.pop(0)
             print('stop action %s wrong pid %r  match %r' % (str(fun.fun), wrong_pid, fun.match_pid))
             if not (wrong_pid and fun.match_pid):
-                fun.run(self.flist, wrong_pid=wrong_pid)
+                fun.run(self.flist, wrong_pid=wrong_pid, cb_param=cb_param)
 
     def getBreaks(self):
         return self.break_addrs
