d0f67cb9b7cdd7cc4e49d0a140d583814f67bd7d
Mike Thompson <mfthomps@nps.edu>
2021-11-30 16:11:59 -0800

    show two sets of bb hits in IDA

---
+++ simics/bin/idaDiff.sh
@@ -0,0 +1,40 @@
+#
+# Start IDA for a given target...
+#
+# The target file (to be disassembled) must be the first argument.
+# The next 2 arguments are coverage files.
+#
+if [ -z "$IDA_DIR" ]; then
+    echo "IDA_DIR not defined."
+    exit
+fi
+if [ -z "$RESIM_DIR" ]; then
+    echo "RESIM_DIR not defined."
+    exit
+fi
+if [ -z "$RESIM_IDA_DATA" ]; then
+    echo "RESIM_IDA_DATA not defined."
+    exit
+fi
+if [[ ! -f "$IDA_DIR/idc/runRESim.idc" ]]; then
+    echo "Copying runRESim.idc to the IDA directory at $IDA_DIR/idc"
+    cp $RESIM_DIR/simics/ida/runRESim.idc $IDA_DIR/idc
+fi
+idacmd=$IDA_DIR/ida
+target=$1
+target_base="$(basename -- $target)"
+shift 1
+
+target_path=$(realpath $target)
+ida_db_path=$RESIM_IDA_DATA/$target_base/$target_base.idb
+echo "target is $target"
+echo "dbpath $ida_db_path"
+remain="$@"
+echo "remain is $remain"
+
+if [[ -f $ida_db_path ]];then
+    #$idacmd -S"$RESIM_DIR/simics/ida/RESimHotKey.idc $target_path $@" $ida_db_path
+    $idacmd -S"$RESIM_DIR/simics/ida/diffBlocks.py $remain" $ida_db_path
+else
+    echo "No IDA db at $ida_db_path."
+fi
