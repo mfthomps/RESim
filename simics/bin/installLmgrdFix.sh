d174929c2a99d8cd7a2757fa12612c1b7a651f1b
Mike <mfthomps@nps.edu>
2022-01-28 18:45:00 -0800

    move to setup

--- simics/bin/installLmgrdFix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "RESIM_DIR is $RESIM_DIR"
-sudo cp $RESIM_DIR/simics/setup/lmgrdFix /usr/bin/
-gotit=$(grep lmgrdFix /etc/rc.local)
-if [[ -z "$gotit" ]]; then
-    echo '/usr/bin/lmgrdFix' | sudo tee -a /etc/rc.local
-    sudo /usr/bin/lmgrdFix
-fi
-
