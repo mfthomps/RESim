a640b4d313a19748d3b8495adc661f1469bf91de
Mike <mfthomps@nps.edu>
2022-02-02 10:00:33 -0800

    add logging, never a master if remote

--- simics/monitorCore/runAFL.py
+++ simics/monitorCore/runAFL.py
@@ -19,25 +19,28 @@ import glob
 import threading
 import select
 import aflPath
-def ioHandler(read_array, stop):
+import resimUtils
+def ioHandler(read_array, stop, lgr):
     log = '/tmp/resim.log'
     with open(log, 'wb') as fh:
         while(True):
             if stop():
                 print('ioHandler sees stop, exiting.')
+                lgr.debug('ioHandler sees stop, exiting.')
                 return
             try:
                 r, w, e = select.select(read_array, [], [], 10) 
             except ValueError:
                 print('select error, must be closed.')
+                lgr.debug('select error, must be closed.')
                 return
             for item in r:
                     data = os.read(item.fileno(), 800)
                     fh.write(data+b'\n')
 
-def handleClose(resim_procs, read_array, duration, remote):
+def handleClose(resim_procs, read_array, duration, remote, lgr):
     stop_threads = False
-    io_handler = threading.Thread(target=ioHandler, args=(read_array, lambda: stop_threads))
+    io_handler = threading.Thread(target=ioHandler, args=(read_array, lambda: stop_threads, lgr))
     io_handler.start()
     if remote:
         while True:
@@ -55,11 +58,12 @@ def handleClose(resim_procs, read_array, duration, remote):
         ps.stdin.write(b'quit\n')
     print('did quit')
     print('done')
+    lgr.debug('done')
     stop_threads = True
     for fd in read_array:
         fd.close()
 
-def doOne(afl_path, afl_seeds, afl_out, size_str,port, afl_name, resim_ini, read_array, resim_path, resim_procs, dict_path, timeout):
+def doOne(afl_path, afl_seeds, afl_out, size_str,port, afl_name, resim_ini, read_array, resim_path, resim_procs, dict_path, timeout, lgr):
     afl_cmd = '%s -i %s -o %s %s -p %d %s -R %s' % (afl_path, afl_seeds, afl_out, size_str, port, dict_path, afl_name)
     print('afl_cmd %s' % afl_cmd) 
 
@@ -75,7 +79,7 @@ def doOne(afl_path, afl_seeds, afl_out, size_str,port, afl_name, resim_ini, read
     read_array.append(resim_ps.stdout)
     read_array.append(resim_ps.stderr)
     print('created resim port %d' % port)
-    handleClose(resim_procs, read_array, timeout, False)
+    handleClose(resim_procs, read_array, timeout, False, lgr)
 
     return resim_ps
     
@@ -100,6 +104,7 @@ def main():
         exit(1)
     resim_path = os.path.join(resim_dir, 'simics', 'bin', 'resim')
     hostname = aflPath.getHost()
+    lgr = resimUtils.getLogger('runAFL', '/tmp/', level=None)
 
     
     here = os.getcwd()
@@ -107,19 +112,19 @@ def main():
     try:
         afl_path = os.path.join(os.getenv('AFL_DIR'), 'afl-fuzz')
     except:
-        print('missing AFL_DIR envrionment variable')
+        lgr.error('missing AFL_DIR envrionment variable')
         exit(1)
     resim_procs = []
     try:
         afl_data = os.getenv('AFL_DATA')
     except:
-        print('missing AFL_DATA envrionment variable')
+        lgr.error('missing AFL_DATA envrionment variable')
         exit(1)
 
     if not args.ini.endswith('.ini'):
         args.ini = args.ini+'.ini'
     if not os.path.isfile(args.ini):
-        print('Ini file %s not found.' % args.ini)
+        lgr.error('Ini file %s not found.' % args.ini)
         exit(1)
     afl_out = os.path.join(afl_data, 'output', afl_name)
     if args.continue_run == True:
@@ -135,7 +140,10 @@ def main():
         os.makedirs(afl_seeds)
     except:
         pass
-    master_slave = '-M'
+    if args.remote:
+        master_slave = '-S'
+    else:
+        master_slave = '-M'
     glist = glob.glob('resim_*/')
 
     if args.tcp:
@@ -163,13 +171,13 @@ def main():
        if os.path.isfile(dpath):
            dict_path = '-x %s' % dpath
        else:
-           print('No dictionary at %s' % dpath)
+           lgr.error('No dictionary at %s' % dpath)
            exit(1)
         
     port = 8700
     read_array = []
     if len(glist) > 0:
-        print('Parallel, doing %d instances' % len(glist))
+        lgr.debug('Parallel, doing %d instances' % len(glist))
         for instance in glist:
             fuzzid = '%s_%s' % (hostname, instance[:-1])
             if not os.path.isdir(instance):
@@ -187,26 +195,26 @@ def main():
             else:
                 cmd = 'xterm -geometry 80x25 -e "%s;sleep 10"' % (afl_cmd)
                 afl_ps = subprocess.Popen(shlex.split(cmd), stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-            print('cmd %s' % cmd) 
+            lgr.debug('cmd %s' % cmd) 
             afl_ps = subprocess.Popen(shlex.split(cmd), stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-            print('created afl')
+            lgr.debug('created afl')
     
             resim_ini = args.ini
             cmd = '%s %s -n' % (resim_path, resim_ini)
             os.environ['ONE_DONE_PARAM'] = str(port)
-            print('cmd is %s' % cmd)
+            lgr.debug('cmd is %s' % cmd)
             resim_ps = subprocess.Popen(shlex.split(cmd), stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
             resim_procs.append(resim_ps)
             read_array.append(resim_ps.stdout)
             read_array.append(resim_ps.stderr)
-            print('created resim port %d' % port)
+            lgr.debug('created resim port %d' % port)
             os.chdir(here)
             master_slave = '-S'
             port = port + 1
-        handleClose(resim_procs, read_array, args.seconds, args.remote)
+        handleClose(resim_procs, read_array, args.seconds, args.remote, lgr)
     else:
-        print('Running single instance')
-        resim_ps = doOne(afl_path, afl_seeds, afl_out, size_str,port, afl_name, args.ini, read_array, resim_path, resim_procs, dict_path, args.seconds)
+        lgr.debug('Running single instance')
+        resim_ps = doOne(afl_path, afl_seeds, afl_out, size_str,port, afl_name, args.ini, read_array, resim_path, resim_procs, dict_path, args.seconds, lgr)
 
   
 if __name__ == '__main__':
