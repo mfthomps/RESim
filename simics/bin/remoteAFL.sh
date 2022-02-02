afcea64c60cb251e5fbe2469bc3bad3316854e8d
Mike Thompson <mfthomps@nps.edu>
2022-02-02 14:33:21 -0800

    pass all args to runAFL

--- simics/bin/remoteAFL.sh
+++ simics/bin/remoteAFL.sh
@@ -6,11 +6,13 @@ if [ "$#" -ne 2 ] || [ "$1" = "-h" ]; then
     exit
 fi
 target=$1
-ini=$2
+shift 1
+ini=$1
+shift 1
 cd $target
 source $HOME/.resimrc
 PATH=$RESIM_DIR/simics/bin:$PATH
 echo "now xrunAFL at target $target"
-echo "$RESIM_DIR/simics/bin/runAFL -r $ini &"
-$RESIM_DIR/simics/bin/runAFL -r $ini &
+echo "$RESIM_DIR/simics/bin/runAFL -r $ini $@ &"
+$RESIM_DIR/simics/bin/runAFL -r $ini $@ &
 
