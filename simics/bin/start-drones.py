905c441e224e4364a7b525a93aaf59a6124be4a1
Mike <mfthomps@nps.edu>
2022-02-02 17:35:21 -0800

    Must include all runAFL arguments; not quite there yet

--- simics/bin/start-drones.py
+++ simics/bin/start-drones.py
@@ -2,17 +2,23 @@
 import os
 import sys
 import subprocess
+import argparse
 import shlex
 resim_dir = os.getenv('RESIM_DIR')
-sys.path.append(os.path.join(resim_dir,'simics', 'monitorCore')
+sys.path.append(os.path.join(resim_dir,'simics', 'monitorCore'))
 import runAFL
 def docmd(cmd):
-    ssh_ps = subprocess.Popen(shlex.split(cmd), stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    ok = True
+    #ssh_ps = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    ssh_ps = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     output = ssh_ps.communicate()
     for line in output[1].decode("utf-8").splitlines():
-         print(line)
+         print("error: "+line)
+         ok = False
     for line in output[0].decode("utf-8").splitlines():
          print(line)
+    if not ok:
+        sys.exit(1)
 
 def main():
     parser = argparse.ArgumentParser(prog='start-drones.py', description='Start drones listed in the drones.txt file.')
@@ -24,11 +30,15 @@ def main():
     parser.add_argument('-m', '--max_bytes', action='store', help='Maximum number of bytes for a write, will truncate AFL genereated inputs.')
     parser.add_argument('-x', '--dictionary', action='store', help='path to dictionary relative to AFL_DIR.')
     parser.add_argument('-f', '--fname', action='store', help='Optional name of shared library to fuzz.')
+    parser.add_argument('-s', '--seconds', action='store', type=int, help='Run for given number of seconds, then exit.')
+    parser.add_argument('-r', '--remote', action='store_true', help='DONT use this, only passed because runAFL needs it.')
     args = parser.parse_args()
     here = os.getcwd()
     base = os.path.basename(here)
     aflout = os.path.join(os.getenv('AFL_DATA'), 'output', base)
     user = os.getenv('USER')
+    resim_dir = os.getenv('RESIM_DIR')
+    remote_afl = os.path.join(resim_dir, 'simics', 'bin', 'remoteAFL.sh')
     try:
         os.rm('/tmp/resimdie.txt')
     except:
@@ -40,18 +50,22 @@ def main():
         for line in fh:
             drone = line.strip()
             cmd = 'ssh %s@%s rm -f /tmp/resimdie.txt' % (user, drone)
+            print('do command: %s' % cmd)
             docmd(cmd)
-            cmd = 'ssh -t %s@%s bash -ic "\'source ~/.resimrc\';/usr/bin/nohup $RESIM_DIR/simics/bin/remoteAFL.sh %s %s"' % (user, drone, here, args.ini)
-            ssh_ps = subprocess.Popen(shlex.split(cmd), stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+            cmd = 'ssh -o LogLevel=QUIET -t %s@%s bash -ic "\'source ~/.resimrc\';/usr/bin/nohup %s %s %s"' % (user, drone, remote_afl, here, args.ini)
+            print('do command: %s' % cmd)
             docmd(cmd)
 
-
-    docmd('get-tars.sh 30 &')
+    get_path = os.path.join(resim_dir,'simics','bin','get-tars.sh')
+    cmd = '%s 30 &'
+    os.system(cmd)
+    print('Now call runAFL')
     runAFL.runAFL(args)
     print("Back from runAFL")
     with open('/tmp/resimdie.txt', 'w') as fh:
         fh.write('die')
-    docmd(stop-drones.sh)
-    docmd("kill $(ps aux | grep '[g]et-tars.sh' | awk '{print $2}')")
+    docmd('stop-drones.sh')
+    os.system("kill $(ps aux | grep '[g]et-tars.sh' | awk '{print $2}')")
+
 if __name__ == '__main__':
     sys.exit(main())
