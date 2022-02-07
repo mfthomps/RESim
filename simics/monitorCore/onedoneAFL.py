1d911e3fe4c5fd0d55068b5b0268e82ad883bd4c
Mike <mfthomps@nps.edu>
2022-02-07 13:57:42 -0800

    Add logging

--- simics/monitorCore/onedoneAFL.py
+++ simics/monitorCore/onedoneAFL.py
@@ -10,11 +10,16 @@ def onedone(top):
     fname=os.getenv('ONE_DONE_PARAM4')
     linear=os.getenv('ONE_DONE_PARAM5')
     is_linear = False
-    if linear is not None and linear.lower()=='true':
-        is_linear=True
-    if protocol == 'tcp': 
-        top.aflTCP(port=port)
-    elif dead == 'TRUE':
-        top.afl(port=port, dead=True, linear=is_linear)
-    else:
-        top.afl(port=port, fname=fname, linear=is_linear)
+    with open('logs/oneDoneAFL.log', 'w') as fh:
+        fh.write('in onedone of oneDoneAFL\n') 
+        if linear is not None and linear.lower()=='true':
+            is_linear=True
+        if protocol == 'tcp': 
+            fh.write('call aflTCP\n')
+            top.aflTCP(port=port)
+            fh.write('back from call aflTCP')
+        else:
+            fh.write('call afl\n')
+            top.afl(port=port, fname=fname, linear=is_linear)
+            fh.write('back from afl\n')
+
