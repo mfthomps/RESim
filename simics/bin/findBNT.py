ad9273d6fb1d319a57595cf41f9f9bce2c3c76e3
mike <mfthomps@nps.edu>
2022-02-14 11:29:08 -0800

    ignore non-text basic blocks, e.g., plt.  Add quiet option

--- simics/bin/findBNT.py
+++ simics/bin/findBNT.py
@@ -6,59 +6,70 @@ import os
 import glob
 import json
 import argparse
-def getIdaData(prog):
-    retval = None
-    resim_ida_data = os.getenv('RESIM_IDA_DATA')
-    if resim_ida_data is None:
-        print('RESIM_IDA_DATA not defined')
-        exit(1)
-    else: 
-        retval = os.path.join(resim_ida_data, prog, prog)
-    return retval
+resim_dir = os.getenv('RESIM_DIR')
+sys.path.append(os.path.join(resim_dir, 'simics', 'monitorCore'))
+import resimUtils
+import elfText
+'''
+Find BNT's by looking at AFL coverage files.
+'''
 
-def findBNT(hits, fun_blocks):
+def findBNT(hits, fun_blocks, quiet, prog_elf):
+    retval = []
     for bb in fun_blocks['blocks']:
         for bb_hit in hits:
             if bb_hit == bb['start_ea']:
+                if bb_hit < prog_elf.address or bb_hit > (prog_elf.address + prog_elf.size):
+                    continue
                 for branch in bb['succs']:
                     if branch not in hits:
-                        print('function: %s branch 0x%x from 0x%x not in hits' % (fun_blocks['name'], branch, bb_hit))
+                        if not quiet:
+                            print('function: %s branch 0x%x from 0x%x not in hits' % (fun_blocks['name'], branch, bb_hit))
+                        entry = {}
+                        entry['bnt'] = branch
+                        entry['source'] = bb_hit
+                        retval.append(entry)
+    return retval
 
-def aflBNT(prog, target, fun_name=None):
-    ida_path = getIdaData(prog)
+def aflBNT(prog, target, fun_name=None, quiet=False):
+    ida_path = resimUtils.getIdaData(prog)
     print('prog: %s  ida_path is %s' % (prog, ida_path))
-    if True:
-        if target is None:
-            fname = '%s.hits' % ida_path
-        else:
-            fname = '%s.%s.hits' % (ida_path, target)
-            print('Using hits file %s' % fname)
-        ''' hits are now just flat lists without functions '''
-        hits = json.load(open(fname))
-        data_path = ida_path+'.prog'
-        with open(data_path) as fh:
-            lines = fh.read().strip().splitlines()
-            print('num lines is %d' % len(lines))
-            prog_file = lines[0].strip()
-        block_file = prog_file+'.blocks'
-        blocks = json.load(open(block_file))
+    bnt_list = []
+    if target is None:
+        fname = '%s.hits' % ida_path
+    else:
+        fname = '%s.%s.hits' % (ida_path, target)
+        print('Using hits file %s' % fname)
+    ''' hits are now just flat lists without functions '''
+    with open(fname) as fh:
+        hits = json.load(fh)
+    prog_file = resimUtils.getProgPath(prog)
+    prog_elf = elfText.getTextOfText(prog_file)
+    print('prog addr 0x%x size %d' % (prog_elf.address, prog_elf.size))
+    block_file = prog_file+'.blocks'
+    with open(block_file) as fh:
+        blocks = json.load(fh)
+    if not quiet:
         print('aflBNT found %d hits and %d blocks' % (len(hits), len(blocks)))
-        if fun_name is None:
-            for fun in blocks:
-                findBNT(hits, blocks[fun])
-        else:
-            for fun in blocks:
-                if blocks[fun]['name'] == fun_name:
-                    findBNT(hits, blocks[fun])
-                    break
+    if fun_name is None:
+        for fun in blocks:
+            this_list = findBNT(hits, blocks[fun], quiet, prog_elf)
+            bnt_list.extend(this_list)
+    else:
+        for fun in blocks:
+            if blocks[fun]['name'] == fun_name:
+                this_list = findBNT(hits, blocks[fun], quiet, prog_elf)
+                bnt_list.extend(this_list)
+                break
+    return bnt_list
 
 def main():
-    parser = argparse.ArgumentParser(prog='findBNT', description='Show branches not taken.')
+    parser = argparse.ArgumentParser(prog='findBNT', description='Show branches not taken for a given program.')
     parser.add_argument('prog', action='store', help='The target program')
     parser.add_argument('-t', '--target', action='store', help='Optional target name, e.g., name of the workspace.')
     parser.add_argument('-f', '--function', action='store', help='Optional function name')
     args = parser.parse_args()
-    aflBNT(args.prog, args.target, args.function)
+    aflBNT(args.prog, args.target, fun_name=args.function)
 
 if __name__ == '__main__':
     sys.exit(main())
