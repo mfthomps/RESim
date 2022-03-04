bb0c766b975580c942eb8e0e80c760cc183a70c1
Mike Thompson <mfthomps@nps.edu>
2022-03-03 17:14:07 -0800

    include crashes in archive

--- simics/bin/arch-tars.sh
+++ simics/bin/arch-tars.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [ ! -f drones.txt ]; then
-   echo "No drones.txt file found"
-   exit
+   echo "No drones.txt file found, archiving local finds."
+   flist=""
+else
+   flist=$(cat drones.txt)
 fi
 if [ "$#" -ne 1 ]; then
     echo "arch-tars.sh <project>"
@@ -9,11 +11,10 @@ if [ "$#" -ne 1 ]; then
     echo "   All data will be in a subdirectory having the workspace name."
     exit
 fi
-if [ ! -d .workspace-properties ]; then
+if [ ! -d .workspace-properties ] && [ ! -d .project-properties ]; then
     echo "This does not look like a Simics workspace."
     exit
 fi
-flist=$(cat drones.txt)
 here=$(pwd)
 base=$(basename $here)
 aflout=$AFL_DATA/output/$base
@@ -23,22 +24,31 @@ mkdir -p $destination
 #
 #  Sync each drone's queues into this master queue.
 #
+echo "do sync"
 for f in $flist; do
     echo "Get sync dir from $f"
-    ssh $USER@$f -o StrictHostKeyChecking=no bash -c "cd .;cd $aflout && tar -czf - $f_resim*/[qf]*" >/tmp/host_$f.tgz
+    #ssh $USER@$f -o StrictHostKeyChecking=no bash -c "cd .;cd $aflout && tar -czf - $f_resim*/[qf]*" >/tmp/host_$f.tgz
+    ssh $USER@$f -o StrictHostKeyChecking=no bash -c "cd .;cd $aflout && tar -czf - $f_resim*/[qf]* $f_resim*/[cf]*" >/tmp/host_$f.tgz
     tar -C $aflout -xf /tmp/host_$f.tgz 
 done
+echo "done sync"
 #
 #  Then tar all the local queues to the archive
 #
+echo "create tar"
 here=$(pwd)
 cd $aflout
 tar -czf $destination/sync_dirs.tgz *_resim_* 
+echo "finished tar"
 cd $here
 aflseed=$AFL_DATA/seeds/$base
 seed_dest=$fuzz_archive/afl/seeds
 mkdir -p $seed_dest
 cp -a $aflseed/* $seed_dest/
 
-workspace_dest=$fuzz_archive/workspace.tgz
-tar -czf $workspace_dest . --exclude=logs --exclude='*.tgz'
+echo "archive workspace"
+workspace_dest=$fuzz_archive/workspace.tar
+tar -cvf /tmp/workspace.tar . --exclude=logs --exclude='*.tgz'
+echo "tar done now copy"
+cp /tmp/workspace.tar $workspace_dest
+echo "done"
