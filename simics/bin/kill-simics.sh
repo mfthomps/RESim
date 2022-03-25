a44157d7debec3cc242698ce066c8ec0f6733620
Mike Thompson <mfthomps@nps.edu>
2022-03-25 09:56:34 -0700

    kill -9

--- simics/bin/kill-simics.sh
+++ simics/bin/kill-simics.sh
@@ -1,2 +1,2 @@
-kill $(ps aux | grep '[l]aunchRESim.py' | awk '{print $2}')
-kill $(ps aux | grep '[r]unAFL' | awk '{print $2}')
+kill -9 $(ps aux | grep '[l]aunchRESim.py' | awk '{print $2}')
+kill -9 $(ps aux | grep '[r]unAFL' | awk '{print $2}')
