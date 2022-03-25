1d96dbec0055c44087627d2472d953157d989b6d
Mike Thompson <mfthomps@nps.edu>
2022-03-25 09:57:06 -0700

    option for magic instruction

--- simics/bin/drive-driver.py
+++ simics/bin/drive-driver.py
@@ -17,6 +17,7 @@ import resimUtils
 def main():
     parser = argparse.ArgumentParser(prog='drive-driver.py', description='Send files to the driver and from there to one or more targets.')
     parser.add_argument('directives', action='store', help='File containing driver directives')
+    parser.add_argument('-n', '--no_magic', action='store_true', help='Do not execute magic instruction.')
     args = parser.parse_args()
     if not os.path.isfile(args.directives):
         print('No file found at %s' % args.directives)
@@ -32,25 +33,30 @@ def main():
 
     remote_directives_file = '/tmp/directives.sh'
     driver_file = open(remote_directives_file, 'w')
-    driver_file.write('/tmp/simics-magic\n')
+    driver_file.write('sleep 2\n')
+    if not args.no_magic:
+        driver_file.write('/tmp/simics-magic\n')
     with open(args.directives) as fh:
         for line in fh:
             if line.strip().startswith('#'):
                 continue
             parts = line.split()
-            if len(parts) != 4:
+            if len(parts) == 2 and parts[0] == 'sleep':
+                driver_file.write(line)
+            elif len(parts) != 4:
                 print('Invalid driver directive: %s' % line)
                 print('    iofile ip port header')
                 exit(1)
-            iofile = parts[0]
-            ip = parts[1]
-            port = parts[2]
-            header = parts[3]
-            base = os.path.basename(iofile)
-            directive = '/tmp/clientudpMult  %s %s %s /tmp/%s' % (ip, port, header, base)
-            driver_file.write(directive+'\n')
-            cmd = 'scp -P 4022 %s  localhost:/tmp/' % iofile
-            os.system(cmd)
+            else:
+                iofile = parts[0]
+                ip = parts[1]
+                port = parts[2]
+                header = parts[3]
+                base = os.path.basename(iofile)
+                directive = '/tmp/clientudpMult  %s %s %s /tmp/%s' % (ip, port, header, base)
+                driver_file.write(directive+'\n')
+                cmd = 'scp -P 4022 %s  localhost:/tmp/' % iofile
+                os.system(cmd)
 
     driver_file.close()
 
@@ -59,7 +65,7 @@ def main():
 
     cmd = 'scp -P 4022 %s  localhost:/tmp/' % remote_directives_file
     os.system(cmd)
-    cmd = 'ssh -p 4022 mike@localhost %s' % remote_directives_file
+    cmd = 'ssh -p 4022 mike@localhost "nohup %s > /dev/null 2>&1 &"' % remote_directives_file
     os.system(cmd)
 
 if __name__ == '__main__':
