08be9e50ecaaec9311ca63efa295678a6d4299cd
Mike Thompson <mfthomps@nps.edu>
2022-08-08 13:14:18 -0700

    diagnostics

--- simics/monitorCore/resimHaps.py
+++ simics/monitorCore/resimHaps.py
@@ -9,23 +9,33 @@ def RES_hap_delete_callback_id(hap_type, hap_num):
     #print('done')
 
 def RES_hap_add_callback(hap_type, callback, param):
+    #print('add hap callback')
     retval = SIM_hap_add_callback(hap_type, callback, param)
+    #print('done')
     return retval
 
 def RES_hap_add_callback_obj(hap_type, cell, val, callback, val2):
+    #print('add hap eh')
     retval = SIM_hap_add_callback_obj(hap_type, cell, val, callback, val2)
+    #print('done')
     return retval 
 
 def RES_hap_add_callback_obj_index(hap_type, cell, val, callback, val2, val3):
+    #print('add hap index')
     retval = SIM_hap_add_callback_obj_index(hap_type, cell, val, callback, val2, val3)
+    #print('done')
     return retval
 
 def RES_hap_add_callback_obj_range(hap_type, cell, val, callback, val2, val3, maxval):
+    #print('add hap range')
     retval = SIM_hap_add_callback_obj_range(hap_type, cell, val, callback, val2, val3, maxval)
+    #print('done')
     return retval
 
 def RES_hap_add_callback_index(hap_type, callback, param, breaknum):
+    #print('add hap on break %d' % breaknum)
     retval = SIM_hap_add_callback_index(hap_type, callback, param, breaknum)
+    #print('done')
     return retval
 
 def RES_delete_breakpoint(bp):
@@ -37,4 +47,6 @@ def RES_delete_breakpoint(bp):
         frameinfo = inspect.getframeinfo(record[0])
         print(frameinfo.function)
     '''
+    #print('del breakpoint')
     SIM_delete_breakpoint(bp)
+    #print('done')
