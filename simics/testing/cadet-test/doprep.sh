6c58dbfba023e643a792901e385b6ef385e99818
Mike Thompson <mfthomps@nps.edu>
2022-11-03 17:12:22 -0700

    xdotool timing fu, tack more time onto fuzzing session.

--- simics/testing/cadet-test/doprep.sh
+++ simics/testing/cadet-test/doprep.sh
@@ -1,6 +1,6 @@
 sleep 8
 ./findWindow.sh "cadet01-tst"
-sleep 2
+sleep 1
 xdotool type "@cgc.debugSnap()"
 xdotool key Return
 xdotool type "run-command-file doprep.simics"
@@ -8,11 +8,13 @@ xdotool key Return
 SIM4=4.8
 if [[ "$SIMDIR" != *"$SIM4"* ]]; then
     ./findWindow.sh "Simics Target Consoles"
+    sleep 1
     xdotool key Down
     xdotool key Return
 fi
-sleep 2
+sleep 1
 ./findWindow.sh driver.mb.sb.com
+sleep 1
 xdotool type "perl -E 'say \"X\" x 1000' | nc 10.0.0.91 5001"
 xdotool key Return
 #
--- simics/testing/cadet-test/dotrack.sh
+++ simics/testing/cadet-test/dotrack.sh
@@ -1,5 +1,6 @@
 sleep 3
 ./findWindow.sh "cadet01-tst"
+sleep 1
 xdotool type "run-command-file dotrack.simics"
 xdotool key Return
 
