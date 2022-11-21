325c7ba2ce9982208bcf39e3e6b4a08985acdadb
Mike Thompson <mfthomps@nps.edu>
2022-11-21 13:56:53 -0800

    diagnostics, deprecate threading

--- simics/monitorCore/runTrack.py
+++ simics/monitorCore/runTrack.py
@@ -31,15 +31,20 @@ def handler(signum, frame):
     print('sig handler with %d' % signum)
     stop_threads = True
 
-def oneTrack(afl_list, resim_path, resim_ini, only_thread, stop_threads, lgr):
+def oneTrack(afl_list, resim_path, resim_ini, only_thread, stop_threads, lgr, instance_path):
+    if instance_path is not None:
+        os.chdir(instance_path)
     here = os.getcwd()
     workspace = os.path.basename(here)
+    lgr.debug('oneTrack, workspace: %s' % workspace)
     log = '/tmp/resim-%s.log' % workspace
     if only_thread:
         os.environ['ONE_DONE_PARAM2']='True'
     with open(log, 'wb') as fh:
         for f in afl_list:
-            os.chdir(here)
+            #os.chdir(here)
+            now_here = os.getcwd()
+            lgr.debug('oneTrack, here: %s, cwd says %s' % (here, now_here))
             os.environ['ONE_DONE_PATH'] = f
             base = os.path.basename(f)
             tdir = os.path.dirname(os.path.dirname(f))
@@ -50,7 +55,7 @@ def oneTrack(afl_list, resim_path, resim_ini, only_thread, stop_threads, lgr):
                 pass
             trackoutput = os.path.join(tdir, 'trackio', base)
             if os.path.isfile(trackoutput):
-                lgr.debug('path exists, skip it %s' % trackoutput)
+                lgr.debug('%s path exists, skip it %s' % (workspace, trackoutput))
                 continue
             try:
                 os.open(trackoutput, os.O_CREAT | os.O_EXCL)
@@ -62,7 +67,7 @@ def oneTrack(afl_list, resim_path, resim_ini, only_thread, stop_threads, lgr):
             #result = os.system('%s %s -n' % (resim_path, resim_ini))
             cmd = '%s %s -n' % (resim_path, resim_ini)
             now_here = os.getcwd()
-            print("starting monitor from %s without UI cmd: %s" % (now_here, cmd))
+            print("%s: starting monitor from %s without UI cmd: %s" % (workspace, now_here, cmd))
             lgr.debug("%s starting monitor from %s without UI cmd: %s" % (now_here, workspace, cmd))
             resim_ps = subprocess.Popen(shlex.split(cmd), stdin=subprocess.PIPE, stdout=fh,stderr=fh)
             resim_ps.wait()
@@ -76,7 +81,7 @@ def oneTrack(afl_list, resim_path, resim_ini, only_thread, stop_threads, lgr):
 def main():
     global stop_threads
     lgr = resimUtils.getLogger('runTrack', '/tmp/', level=None)
-    here= os.path.dirname(os.path.realpath(__file__))
+    script_path= os.path.dirname(os.path.realpath(__file__))
     parser = argparse.ArgumentParser(prog='runTrack', description='Run injectIO on all sessions in a target found by AFL.')
     parser.add_argument('ini', action='store', help='The RESim ini file used during the AFL session.')
     parser.add_argument('target', action='store', help='The afl output directory relative to AFL_OUTPUT in the ini file, or AFL_DATA in bashrc.')
@@ -109,7 +114,7 @@ def main():
                     os.remove(track_file) 
 
     ''' The script to be called by RESim once it is initialized '''
-    os.environ['ONE_DONE_SCRIPT'] = os.path.join(here, 'onedoneTrack.py')
+    os.environ['ONE_DONE_SCRIPT'] = os.path.join(script_path, 'onedoneTrack.py')
     resim_path = os.path.join(os.getenv('RESIM_DIR'), 'simics', 'bin', 'resim')
     glist = glob.glob('resim_*/')
     thread_list = []
@@ -118,18 +123,19 @@ def main():
     signal.signal(signal.SIGTERM, handler)
     here = os.getcwd()
     if len(glist) > 0: 
+        ''' TBD remove this, runTrack now creates multiple processes.  The mess below causes os.cwd issues in threads'''
         print('Parallel, doing %d instances' % len(glist))
+        lgr.debug('Parallel, doing %d instances' % len(glist))
         for instance in glist:
-            if not os.path.isdir(instance):
-                continue
-            os.chdir(instance)
-            lgr.debug('start oneTrack from workspace %s' % instance)
-            track_thread = threading.Thread(target=oneTrack, args=(afl_list, resim_path, resim_ini, args.only_thread, lambda: stop_threads, lgr))
+            instance_path = os.path.join(here, instance)
+            lgr.debug('start oneTrack from workspace %s' % instance_path)
+            track_thread = threading.Thread(target=oneTrack, args=(afl_list, resim_path, resim_ini, args.only_thread, lambda: stop_threads, lgr, instance_path))
             thread_list.append(track_thread)
             track_thread.start()
-            os.chdir(here)
     else:
-        track_thread = threading.Thread(target=oneTrack, args=(afl_list, resim_path, resim_ini, args.only_thread, lambda: stop_threads, lgr))
+        print('Single instance')
+        lgr.debug('Single instance')
+        track_thread = threading.Thread(target=oneTrack, args=(afl_list, resim_path, resim_ini, args.only_thread, lambda: stop_threads, lgr, None))
         thread_list.append(track_thread)
         track_thread.start()
    
