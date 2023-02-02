6c694f262241233b4587a8abbea20144bf1dcd48
Mike Thompson <mfthomps@nps.edu>
2023-02-01 16:26:57 -0800

    diagnostics

--- simics/bin/findTrack.py
+++ simics/bin/findTrack.py
@@ -22,26 +22,41 @@ def getQueue(f):
     track = os.path.join(os.path.dirname(cover), 'queue', base)
     return track
 
-def findTrack(f, addr):
+def findTrack(f, addr, one):
+    retval = False
     track_path = getTrack(f)
     queue_path = getQueue(f)
+    mark_cycle = 0
+    print('NEW FIND TRACK %s' % track_path)
     if os.path.isfile(track_path):
         track = json.load(open(track_path))
         mark_list = track['marks']
         #print('%d marks' % len(mark_list))
         count = 1
         for mark in mark_list:
+            print('%d ip: 0x%x cycle: 0x%x' % (count, mark['ip'], mark['cycle']))
+            if mark['cycle'] < mark_cycle:
+                print('OUT OF ORDER')
+                print('mark cycle 0x%x' % mark['cycle'])
+                break
+            else:
+                mark_cycle = mark['cycle'] 
             if mark['mark_type'] == 'read' and mark['ip']==addr:
                 size = os.path.getsize(queue_path)
                 print('0x%x found at mark %d in (len %d)  %s (TBD, why off by one?)' % (addr, count, size, queue_path))
+                retval = True
+                if one:
+                    break
             count += 1
     else:
         print('not a file: %s' % track_path)
+    return retval
 
 def main():
     parser = argparse.ArgumentParser(prog='findTrack', description='Find track files that reference an input at a given instruction address')
     parser.add_argument('target', action='store', help='The AFL target, generally the name of the workspace.')
     parser.add_argument('addr', action='store', help='The instruction address.')
+    parser.add_argument('-o', '--one', action='store_true', help='stop after one.')
     args = parser.parse_args()
 
     afl_path = os.getenv('AFL_DATA')
@@ -51,7 +66,9 @@ def main():
     print('got %d paths' % len(expaths))
     addr = int(args.addr, 16) 
     for index in range(len(expaths)):
-            findTrack(os.path.join(target_path, expaths[index]), addr)
+        result = findTrack(os.path.join(target_path, expaths[index]), addr, args.one)
+        if result and args.one:
+            break
 
 if __name__ == '__main__':
     sys.exit(main())
