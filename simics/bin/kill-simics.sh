54acab355ab28eec7307463b6409a3d3e8567572
Mike Thompson <mfthomps@nps.edu>
2023-02-23 11:14:46 -0800

    supress errors

--- simics/bin/kill-simics.sh
+++ simics/bin/kill-simics.sh
@@ -1,3 +1,3 @@
-kill -9 $(ps aux | grep '[l]aunchRESim.py' | awk '{print $2}')
-kill -9 $(ps aux | grep '[r]unAFL' | awk '{print $2}')
-kill -9 $(ps aux | grep '[s]imics-common' | awk '{print $2}')
+kill -9 $(ps aux | grep '[l]aunchRESim.py' | awk '{print $2}') >>/dev/null 2>&1
+kill -9 $(ps aux | grep '[r]unAFL' | awk '{print $2}') >>/dev/null 2>&1
+kill -9 $(ps aux | grep '[s]imics-common' | awk '{print $2}') >>/dev/null 2>&1
