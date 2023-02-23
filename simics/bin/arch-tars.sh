1123b4148881ac4e572f5a45b56aae1420515e26
Mike Thompson <mfthomps@nps.edu>
2023-02-23 11:15:25 -0800

    change location of manual artifacts

--- simics/bin/arch-tars.sh
+++ simics/bin/arch-tars.sh
@@ -38,7 +38,7 @@ echo "done sync"
 echo "create tar"
 here=$(pwd)
 cd $aflout
-tar -czf $destination/sync_dirs.tgz *_resim_* manual coverage 
+tar -czf $destination/sync_dirs.tgz *_resim_* manual_queue manual_coverage manual_trackio
 echo "finished tar"
 cd $here
 aflseed=$AFL_DATA/seeds/$base
