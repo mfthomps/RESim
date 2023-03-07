41436a3a3dc2389b3702757d8431d145c7212a88
Mike Thompson <mfthomps@nps.edu>
2023-03-07 09:02:00 -0800

    first argument is the program name, there is no target argument

--- simics/bin/syncIda.sh
+++ simics/bin/syncIda.sh
@@ -8,22 +8,22 @@ if [ -z "$RESIM_IDA_DATA" ]; then
     exit
 fi
 if [ $# -lt 2 ] || [ $1 = "-h" ]; then
-    echo "syncIda.sh <target> <server> [user]"
+    echo "syncIda.sh <program> <server> [user]"
     echo "provide the optional user if id on remote differs from local."
     exit
 fi
-target=$1
-target_base="$(basename -- $target)"
+program=$1
+program_base="$(basename -- $program)"
 remote=$2
 if [ $# -eq 3 ]; then
     user=$3@
 else
     user=""
 fi
-remote_ida=$( ssh $user$remote "source $HOME/.resimrc;mkdir -p \$RESIM_IDA_DATA/$target_base; echo \$RESIM_IDA_DATA" )
+remote_ida=$( ssh $user$remote "source $HOME/.resimrc;mkdir -p \$RESIM_IDA_DATA/$program_base; echo \$RESIM_IDA_DATA" )
 if [ -z "$remote_ida" ];then
            echo "The $remote server needs a ~/.resimrc file containing the RESim env variables that may be in your ~/.bashrc file"
            exit 1
 fi
-rsync -avh $user$remote:$remote_ida/$target_base/*.hits $RESIM_IDA_DATA/$target_base/
+rsync -avh $user$remote:$remote_ida/$program_base/*.hits $RESIM_IDA_DATA/$program_base/
 
