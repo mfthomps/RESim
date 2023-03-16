5a7c36819611bca40028be8fb89f8fb4c52bb8a9
Mike Thompson <mfthomps@nps.edu>
2023-03-16 16:58:05 -0700

    add example option

--- simics/bin/resim-ws.sh
+++ simics/bin/resim-ws.sh
@@ -12,16 +12,31 @@ if [[ -z "$RESIM_DIR" ]]; then
    echo "RESIM_DIR not defined"
    exit
 fi
+if [ "$1" == "-h" ]; then
+   echo "resim-ws.sh"
+   echo "   Create a Simics workspace (project) directory and populate it with common RESim files."
+   echo "   Use -e to populate the workspace with example ini files and example param files."
+fi
 $SIMDIR/bin/project-setup  || exit
-cp $RESIM_DIR/simics/workspace/mytarget.ini .
-cp $RESIM_DIR/simics/workspace/clear-linux.ini .
-cp $RESIM_DIR/simics/workspace/clear64.param .
 cp $RESIM_DIR/simics/workspace/driver-script.sh .
-cp $RESIM_DIR/simics/workspace/ubuntu.param .
-cp $RESIM_DIR/simics/workspace/ubuntu.ini .
 cp $RESIM_DIR/simics/workspace/authorized_keys .
+if [ "$1" == "-e" ]; then
+#
+#   Examples
+#
+    cp $RESIM_DIR/simics/workspace/mytarget.ini .
+    cp $RESIM_DIR/simics/workspace/ubuntu_driver.ini .
+    cp $RESIM_DIR/simics/workspace/client.py .
+    cp $RESIM_DIR/simics/workspace/ubuntu.param .
+    cp $RESIM_DIR/simics/workspace/clear-linux.ini .
+    cp $RESIM_DIR/simics/workspace/clear64.param .
+    cp $RESIM_DIR/simics/workspace/mapdriver.simics .
+fi
 here=`pwd`
 echo "Workspace setup at $here."
 if [ ! -d ./targets/qsp-x86 ]; then
+#
+#   Configure for use with Free Simics
+#
     $RESIM_DIR/simics/bin/free-ws.sh
 fi
