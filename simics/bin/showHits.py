c7517bb6783fa651df57756aea43878a58f01afb
Mike Thompson <mfthomps@nps.edu>
2023-04-11 13:49:56 -0700

    alter to just take a file name

--- simics/bin/showHits.py
+++ simics/bin/showHits.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Display a program's hits file as hex
+# Display a givne hits file as hex
 #
 import sys
 import os
@@ -12,20 +12,15 @@ try:
 except:
     import configparser as ConfigParser
 def main():
-    parser = argparse.ArgumentParser(prog='showHits', description='Show program hits file as hex.  Must be run from RESIM_ROOT_PREFIX per ini file.')
-    parser.add_argument('prog', action='store', help='The program that was fuzzed. ') 
+    parser = argparse.ArgumentParser(prog='showHits', description='Show content of a hits file as hex. ')
+    parser.add_argument('hits_file', action='store', help='Name of the hits file')
     args = parser.parse_args()
-    ida_data = os.getenv('RESIM_IDA_DATA')
-    if ida_data is None:
-        print('RESIM_IDA_DATA not defined')
-        exit(1)
-    here = os.path.getcwd()
-    root_dir = os.path.basename(here)
-    hits = os.path.join(ida_data, root_dir, args.prog, args.prog+'.hits')
+    hits = args.hits_file
     jhits = json.load(open(hits))
     print('hits from %s:' % hits)
     for hit in jhits:
         print('\t0x%x' % hit)
+    print('Total hits %d' % len(jhits))
     
          
 
