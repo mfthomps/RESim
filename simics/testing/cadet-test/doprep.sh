3c63bad07560aecbb244fb966537c484a1dfaab4
Mike Thompson <mfthomps@nps.edu>
2023-06-27 16:08:44 -0700

    more restructure to avoid gui

--- simics/testing/cadet-test/doprep.sh
+++ simics/testing/cadet-test/doprep.sh
@@ -1,6 +1,10 @@
 sleep 8
 ./findWindow.sh "cadet01-tst"
 sleep 1
+xdotool type "@cgc.setCommandCallback(cgc.trackKbuf)"
+xdotool key Return
+xdotool type "@cgc.setCommandCallbackParam(4)"
+xdotool key Return
 xdotool type "@cgc.debugSnap()"
 xdotool key Return
 xdotool type "run-command-file doprep.simics"
