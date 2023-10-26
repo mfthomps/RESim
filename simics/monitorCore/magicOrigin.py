f7d77ecc205cb9dc7087ef9d13aadc0912a1d367
mike <mfthomps@nps.edu>
2023-10-26 20:55:03 +0000

    pid to tid

--- simics/monitorCore/magicOrigin.py
+++ simics/monitorCore/magicOrigin.py
@@ -127,7 +127,7 @@ class MagicOrigin():
             self.lgr.debug('MagicOrigin back from calling setOriginWhen stopped, now continue')
             SIM_run_command('c')
         else:
-            self.top.toPid(-1, callback=self.top.setOrigin)
+            self.top.toTid('-1', callback=self.top.setOrigin)
         #self.bookmarks.setOrigin(self.cpu)
         #self.lgr.debug('MagicOrigin, continue')
         #SIM_run_command('c')
