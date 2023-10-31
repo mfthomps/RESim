fd2ca2fb0876345d966fb97574a66556594d4d1f
Mike Thompson <mfthomps@nps.edu>
2023-10-31 13:49:54 -0700

    use physical addresses, create json dead file

--- simics/bin/heavyHitters.py
+++ simics/bin/heavyHitters.py
@@ -6,24 +6,42 @@ Intended for use in blackballing parts of the code from afl.
 import sys
 import os
 import argparse
+import json
 
 def main():
     parser = argparse.ArgumentParser(prog='heavyHitters', description='Find blocks with the most hits in a log file')
     parser.add_argument('log', action='store', help='The log to search.')
+    parser.add_argument('count', type=int, action='store', help='The number of blocks to display.')
     args = parser.parse_args()
     counts = {}
     with open(args.log) as fh:
         for line in fh:
             if 'coverage bbHap addr' in line:
                 parts = line.split()
-                linear =  parts[-3]
-                if linear not in counts:
-                    counts[linear] = 1
+                phys_s = parts[-7]
+                if phys_s.endswith(','):
+                    phys_s = phys_s[:-1]
+                phys =  int(phys_s, 16)
+                #offset =  int(parts[-5], 16)
+                #addr = linear - offset
+                if phys not in counts:
+                    counts[phys] = 1
                 else:
-                    counts[linear] += 1
-    sorted_hits = sorted(counts.items(), key=lambda x:x[1])
+                    counts[phys] += 1
+    sorted_hits = sorted(counts.items(), key=lambda x:x[1], reverse=True)
+    num = 0
+    outlist = []
+    #print(sorted_hits)
     for addr, count in sorted_hits:
-        print('%s  %d' % (addr, count))
+        outlist.append(addr)
+        print('0x%x  %d' % (addr, count))
+        num += 1
+        if num >= args.count:
+            break
+    outfile = '/tmp/hitters.dead' 
+    with open(outfile, 'w') as fh:
+        fh.write(json.dumps(outlist))
+    print('List of addresses stored in %s' % outfile)
 
 if __name__ == '__main__':
     sys.exit(main())
