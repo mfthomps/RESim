501dbc77024961b61b6848bce7c26d4e721b7531
mike <mfthomps@nps.edu>
2023-11-20 23:38:10 +0000

    include time delta for most recent queue file.

--- simics/bin/fuzz-summary.py
+++ simics/bin/fuzz-summary.py
@@ -9,6 +9,8 @@ import glob
 import json
 import argparse
 import subprocess
+from datetime import datetime
+
 try:
     import ConfigParser
 except:
@@ -17,6 +19,21 @@ resim_dir = os.getenv('RESIM_DIR')
 
 sys.path.append(os.path.join(resim_dir, 'simics', 'monitorCore'))
 import aflPath
+
+def getRecentDelta(flist):
+    recent = None
+    for f in flist:
+        dt = os.path.getmtime(f)
+        if recent is None or dt > recent:
+            recent = dt
+    now = datetime.now()
+    now_ts = datetime.timestamp(now)
+    delta = now_ts - recent
+    #print('delta is %d' % delta)
+    #dt = datetime.fromtimestamp(recent)
+    #print('recent is %s' % str(recent))
+    #print(dt.strftime("%m/%d/%Y, %H:%M:%S"))
+    return delta
 
 def main():
     afldir = os.getenv('AFL_DIR')
@@ -32,5 +49,7 @@ def main():
     print('RESim sees %d unique execution paths.' % len(unique_files))
     print('\t %d crashes' % len(crash_files))
     print('\t %d hangs' % len(hang_files))
+    delta_queue = getRecentDelta(queue_files)
+    print('Most recent queue file %d seconds ago' % delta_queue)
 if __name__ == '__main__':
     sys.exit(main())
