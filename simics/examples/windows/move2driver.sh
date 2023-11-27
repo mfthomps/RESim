2f1264215badf10e4e8dd97fc987a61e4fdd45ca
Mike Thompson <mfthomps@nps.edu>
2023-11-27 10:55:10 -0800

    fix env variable name

--- simics/examples/windows/move2driver.sh
+++ simics/examples/windows/move2driver.sh
@@ -3,6 +3,6 @@
 # Sends the executable, ssh keys and a "move_from_driver.sh" script to the driver.
 # It then runs the move_from_driver.sh
 #
-fname=$RESIM_IMAGES/Windows7x64Files/simple_server.exe
+fname=$RESIM_IMAGE/Windows7x64Files/simple_server.exe
 scp -P 4022 id_rsa id_rsa.pub $fname move_from_driver.sh mike@localhost:/tmp/
 ssh -p 4022 mike@localhost "cd /tmp;source ./move_from_driver.sh"
