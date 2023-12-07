6937496e7b45b73de10da62fc7dcd7d4a954f1f7
Mike Thompson <mfthomps@nps.edu>
2023-12-07 14:10:18 -0800

    Use calls and rets to run to the return from the current function.

---
+++ simics/monitorCore/runToReturn.py
@@ -0,0 +1,98 @@
+'''
+ * This software was created by United States Government employees
+ * and may not be copyrighted.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+'''
+'''
+Run forward until a ret instruction is hit (adjusted by any calls).
+
+'''
+from simics import *
+class RunToReturn():
+    def __init__(self, top, cpu, task_utils, kernel_base, context_manager, lgr):
+        self.lgr = lgr
+        self.top = top
+        self.cpu = cpu
+        self.task_utils = task_utils
+        self.kernel_base = kernel_base
+        self.context_manager = context_manager
+        self.eip = top.getEIP()
+        self.tid = top.getTID()
+        self.call_hap = None
+        self.ret_hap = None
+        self.call_count = 1
+        self.ret_count = 0 
+
+        self.setBreaks()
+
+    def setBreaks(self):
+        self.lgr.debug('RunToReturn setBreaks eip 0x%x tid:%s' % (self.eip, self.tid))
+        if self.cpu.architecture == 'arm':
+            prefix = "bl"
+        else:
+            prefix = "call"
+        call_break = self.context_manager.genBreakpoint(None, Sim_Break_Linear, Sim_Access_Execute, 0, self.kernel_base, 0, prefix=prefix)
+        self.call_hap = self.context_manager.genHapIndex("Core_Breakpoint_Memop", self.callHap, None, call_break, 'run_to_return_call')
+
+        prefix = 'ret'
+        ret_break = self.context_manager.genBreakpoint(None, Sim_Break_Linear, Sim_Access_Execute, 0, self.kernel_base, 0, prefix=prefix)
+        self.ret_hap = self.context_manager.genHapIndex("Core_Breakpoint_Memop", self.retHap, None, ret_break, 'run_to_return_ret')
+
+
+    def callHap(self, dumb, context, break_num, memory):
+        if self.call_hap is None:
+            return 
+        tid = self.top.getTID()
+        if tid != self.tid:
+            return
+        self.lgr.debug('RunToReturn callHap calls:%d rets:%d' % (self.call_count, self.ret_count))
+        self.call_count = self.call_count+1
+
+    def retHap(self, dumb, context, break_num, memory):
+        if self.ret_hap is None:
+            return 
+        tid = self.top.getTID()
+        if tid != self.tid:
+            return
+        self.lgr.debug('RunToReturn retHap calls:%d rets:%d' % (self.call_count, self.ret_count))
+        self.ret_count = self.ret_count+1
+        if self.call_count == self.ret_count:
+            SIM_run_alone(self.rmHaps, None)
+            self.top.stopAndGo(self.stepOne)
+
+    def rmHaps(self, dumb):
+        if self.call_hap is not None:
+            self.context_manager.genDeleteHap(self.call_hap)
+            self.call_hap = None
+        if self.ret_hap is not None:
+            self.context_manager.genDeleteHap(self.ret_hap)
+            self.ret_hap = None
+
+    def stepOne(self):
+        SIM_run_alone(self.stepOneAlone, None)
+
+    def stepOneAlone(self, dumb):
+        self.lgr.debug('RunToReturn stepOneAlone')
+        SIM_run_command('pselect %s' % self.cpu.name)
+        SIM_run_command('si')
+        self.top.show()
+        print('done')
