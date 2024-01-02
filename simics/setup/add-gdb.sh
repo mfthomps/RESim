0da89a48aae510d06c45f9a8a0fbf7531ba48e68
Mike Thompson <mfthomps@nps.edu>
2024-01-02 10:00:54 -0800

    script to add gdb-remote package

---
+++ simics/setup/add-gdb.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+#  Add the RESim gdb-remote package, e.g., to support Ghidra debugging of windows targets.
+#
+$SIMDIR/bin/addon-manager -s $RESIM_DIR/simics/packages/simics-resim_gdb-6.0.0
