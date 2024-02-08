15d2045183cc8794ce5bd86b5fd1c289d5a33045
Mike Thompson <mfthomps@nps.edu>
2024-02-08 10:38:39 -0800

    trackIO no longer includes send calls

--- simics/testing/windows/check_track.sh
+++ simics/testing/windows/check_track.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-result=$(grep "11 b'Read 16" my_test.wm)
+result=$(grep "6 b'Read 16" my_test.wm)
 if [ -z "$result" ]; then
-    echo "failed to find Read 16 at entry 11 in watch marks"
+    echo "failed to find Read 16 at entry 6 in watch marks"
     exit 1
 fi
 echo "Track test passed."
