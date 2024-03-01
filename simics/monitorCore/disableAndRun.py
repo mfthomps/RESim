f5cca4ef17548ee3af03a639c4445368d3f62d7e
Mike Thompson <mfthomps@nps.edu>
2024-02-29 19:49:54 -0800

    specify direction when disabling breakpoints

--- simics/monitorCore/disableAndRun.py
+++ simics/monitorCore/disableAndRun.py
@@ -1,3 +1,31 @@
+'''
+ * This software was created by United States Government employees
+ * and may not be copyrighted.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+'''
+'''
+Disable all breakpoints at the context_manager, run to an address and
+then re-enable and call an optional callback.
+'''
 from simics import *
 from resimHaps import *
 class DisableAndRun():
@@ -7,7 +35,7 @@ class DisableAndRun():
         self.lgr = lgr
         self.context_manager = context_manager
         self.callback = callback
-        self.lgr.debug('DisableAndRun for 0x%x' % addr)
+        self.lgr.debug('DisableAndRun for 0x%x cycles: 0x%x' % (addr, self.cpu.cycles))
         SIM_run_alone(self.setBreakHap, addr)
 
     def setBreakHap(self, addr):
@@ -18,13 +46,13 @@ class DisableAndRun():
         else: 
             self.addr_break = SIM_breakpoint(self.cpu.physical_memory, Sim_Break_Physical, Sim_Access_Execute, phys_block.address, 1, 0)
             self.addr_hap = RES_hap_add_callback_index("Core_Breakpoint_Memop", self.hitAddr, None, self.addr_break)
-            self.context_manager.disableAll()
+            self.context_manager.disableAll(direction='forward')
             self.lgr.debug('DisableAndRun for 0x%x, set break and disabled context manager breaks' % addr)
  
-    def hitAddr(self, deumb, the_object, the_break, memory):
+    def hitAddr(self, dumb, the_object, the_break, memory):
         if self.addr_hap is not None:
             self.lgr.debug('DisableAndRun hit break 0x%x' % self.addr)
-            self.context_manager.enableAll()
+            SIM_run_alone(self.context_manager.enableAll, None)
             hap = self.addr_hap
             RES_delete_breakpoint(self.addr_break)
             self.addr_break = None
