d1af4d166a1da05e746f2be06cf392c745504af4
Mike <mfthomps@nps.edu>
2024-05-17 12:50:39 -0700

    traceall change introduced bugs, only automaze and manage own run if traceall.

--- simics/monitorCore/onedoneTrack.py
+++ simics/monitorCore/onedoneTrack.py
@@ -40,18 +40,21 @@ def onedone(top):
     fname=os.getenv('ONE_DONE_PARAM5')
     trace_all_s=os.getenv('ONE_DONE_PARAM6')
     trace_all = False
+    run = True
     no_trace_dbg = False
     if trace_all_s is not None and trace_all_s.lower() == 'true':
         trace_all = True
         no_trace_dbg = True
+        run = False
 
     myinject = top.injectIO(path, save_json=outpath, callback=quit, go=False, only_thread=only_thread, no_page_faults=no_page_faults, 
-                      max_marks=max_marks, fname=fname, trace_all=trace_all, no_trace_dbg=no_trace_dbg, run=False)
+                      max_marks=max_marks, fname=fname, trace_all=trace_all, no_trace_dbg=no_trace_dbg, run=run)
     myinject.setExitCallback(reportExit)
     myinject.go()
-    top.autoMaze()
-    run_cycles = 1000000
-    SIM_continue(run_cycles)
-    top.quit()
+    if trace_all:
+        top.autoMaze()
+        run_cycles = 1000000
+        SIM_continue(run_cycles)
+        top.quit()
 
 
