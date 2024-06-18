a458164a1b984ed04f0a8c34e09924341629c0dc
Mike <mfthomps@nps.edu>
2024-06-18 10:54:36 -0700

    error handling

--- simics/monitorCore/writeData.py
+++ simics/monitorCore/writeData.py
@@ -210,12 +210,11 @@ class WriteData():
                 #self.lgr.debug('writeData writeKdata, call setCallHap')
                 self.setCallHap()
             while remain > 0:
+                 if index >= len(self.k_bufs):
+                     self.lgr.error('writeKdata index %d out of range with %d bytes remaining.' % (index, remain))
+                     break
                  count = min(self.k_buf_len[index], remain)
                  end = offset + count
-                 if index >= len(self.k_bufs):
-                     self.lgr.error('writeKdata index %d out of range with %d bytes remaining, count was %d.' % (index, remain, count))
-                     self.lgr.debug('writeKdata to buf[%d] data[%d:%d] remain %d' % (index,  offset, end, remain))
-                     break
                  #self.lgr.debug('writeKdata write %d bytes to 0x%x.  k_buf_len is %d' % (len(data[offset:end]), self.k_bufs[index], self.k_buf_len[index]))
                  self.mem_utils.writeString(self.cpu, self.k_bufs[index], data[offset:end])
                  index = index + 1
