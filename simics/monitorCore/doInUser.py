4c8f0bb284b675c8aa7593b6093f7bc86efa825d
Mike Thompson <mfthomps@nps.edu>
2024-10-07 09:21:31 -0700

    utility to perform function once in user space

---
+++ simics/monitorCore/doInUser.py
@@ -0,0 +1,55 @@
+'''
+ * This software was created by United States Government employees
+ * and may not be copyrighted.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+'''
+
+from simics import *
+import resimHaps
+import memUtils
+class DoInUser():
+    def __init__(self, top, cpu, callback, param, task_utils, mem_utils, lgr):
+        self.top = top
+        self.cpu = cpu
+        self.callback = callback
+        self.param = param
+        self.task_utils = task_utils
+        self.mem_utils = mem_utils
+        self.lgr = lgr
+        dum_cpu, comm, self.tid = self.task_utils.curThread()
+        self.mode_hap = SIM_hap_add_callback_obj("Core_Mode_Change", self.cpu, 0, self.modeChanged, self.cpu)
+        self.lgr.debug('doInUser mode hap set for tid:%s' % self.tid)
+
+    def modeChanged(self, cpu, one, old, new):
+        if self.mode_hap is None:
+            return
+        dum_cpu, comm, tid = self.task_utils.curThread()
+        cell_name = self.top.getTopComponentName(self.cpu)
+        #cpl = SIM_processor_privilege_level(self.cpu)
+        cpl = memUtils.getCPL(cpu)
+        self.lgr.debug('doInUser mode_changed %s %s (%s) look for %s, cpl is %d ' % (cell_name, tid, comm, self.tid, cpl))
+        if tid == self.tid and cpl != 0:
+            self.lgr.debug('doInUser mode_changed do callback')
+            self.callback(self.param) 
+            hap = self.mode_hap
+            SIM_run_alone(resimHaps.RES_delete_mode_hap, hap)
+            self.mode_hap = None
