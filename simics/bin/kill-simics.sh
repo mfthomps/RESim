b8f4232ee712750e7ae67393d7839314a93cc8fe
Mike Thompson <mfthomps@nps.edu>
2025-02-11 10:32:19 -0800

    simics 7 changed execute string"

--- simics/bin/kill-simics.sh
+++ simics/bin/kill-simics.sh
@@ -1,3 +1,4 @@
 kill -9 $(ps aux | grep '[l]aunchRESim.py' | awk '{print $2}') >>/dev/null 2>&1
 kill -9 $(ps aux | grep '[r]unAFL' | awk '{print $2}') >>/dev/null 2>&1
-kill -9 $(ps aux | grep '[s]imics-common' | awk '{print $2}') >>/dev/null 2>&1
+#kill -9 $(ps aux | grep '[s]imics-common' | awk '{print $2}') >>/dev/null 2>&1
+kill -9 $(ps aux | grep '[p]y3.simics' | awk '{print $2}') >>/dev/null 2>&1
