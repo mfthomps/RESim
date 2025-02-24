8866aa8fa04b9aee3f029723d84af98ddf80e677
Mike Thompson <mfthomps@nps.edu>
2025-02-24 12:56:52 -0800

    diagnostics

--- simics/monitorCore/genMonitor.py
+++ simics/monitorCore/genMonitor.py
@@ -4625,7 +4625,7 @@ class GenMonitor():
                 quiet=False, mark_logs=False, kbuf=False, call_list=None, run=True, commence=None, offset=None, length=None, commence_offset=0):
         if max_marks is None:
             max_marks = self.max_marks
-        self.lgr.debug('trackIO max_marks %s' % max_marks) 
+        self.lgr.debug('trackIO fd: 0x%x max_marks %s count %d' % (fd, max_marks, count)) 
         if self.bookmarks is None:
             self.lgr.error('trackIO called but no debugging session exists.')
             return
