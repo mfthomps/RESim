d9fe3d41cc9c9ad3bdea1201da4e62231dd9f3e9
Mike Thompson <mfthomps@nps.edu>
2025-03-28 10:50:12 -0700

    diagnostics

--- simics/monitorCore/prepInject.py
+++ simics/monitorCore/prepInject.py
@@ -41,7 +41,7 @@ class PrepInject():
 
     def prepInject(self, dumb=None, ignore_waiting=False):
         ''' Use runToInput to find location of desired input call.  Set callback to instrument the call and return '''
-        self.lgr.debug('prepInject snap %s FD: %d (0x%x)' % (self.snap_name, self.fd, self.fd))
+        self.lgr.debug('prepInject snap %s FD: %d (0x%x) commence: %s' % (self.snap_name, self.fd, self.fd, self.commence))
         ''' passing "cb_param" causes stop function to use parameter passed by the stop hap, which should be the callname '''
         self.top.stopWatchPageFaults()
         f1 = stopFunction.StopFunction(self.instrumentIO, ['cb_param'], nest=False)
