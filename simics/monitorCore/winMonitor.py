e01f3eb4704b965e3d632495b2ade2917d7e1bd9
Mike Thompson <mfthomps@nps.edu>
2025-03-28 10:51:37 -0700

    diagnostics

--- simics/monitorCore/winMonitor.py
+++ simics/monitorCore/winMonitor.py
@@ -246,7 +246,7 @@ class WinMonitor():
             self.lgr.debug('runToIO already in call_traces, add param')
             self.call_traces['runToIO'].addCallParams([call_params])
         else:
-            self.lgr.debug('runToIO on FD %s count %s' % (str(fd), count))
+            self.lgr.debug('runToIO on FD %s count %s sub_match %s' % (str(fd), count, sub_match))
 
             if True:
                 skip_and_mail = True
