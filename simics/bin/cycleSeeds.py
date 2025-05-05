5dcf0483e1b5081ae8eed5d39d9ee8bd1cebc10d
mike <mfthomps@nps.edu>
2025-05-05 15:41:14 +0000

    option to cycle seeds found via progressive fuzzing.

--- simics/bin/cycleSeeds.py
+++ simics/bin/cycleSeeds.py
@@ -9,6 +9,7 @@ import aflPath
 def main():
     parser = argparse.ArgumentParser(prog='cycleSeeds.py', description='Replace afl seed files with those found in the unique coverage file.')
     parser.add_argument('-t', '--target', action='store', help='Optional target name from which to get the queue files (if different from current directory')
+    parser.add_argument('-a', '--auto_seeds', action='store', help='Optional directory name containing additional seeds.')
     args = parser.parse_args()
     here = os.getcwd()
     my_target = os.path.basename(here)
@@ -27,6 +28,12 @@ def main():
         print('got %d unique queue files, copy to %s' % (len(qfiles), seeds_dir))
         for q in qfiles:
             shutil.copy(q, seeds_dir)
+        if args.auto_seeds is not None and os.path.isdir(args.auto_seeds):
+            add_seeds = os.listdir(args.auto_seeds)
+            print('Will add %d seeds from %s' % (len(add_seeds), args.auto_seeds))
+            for f in add_seeds:
+                src = os.path.join(args.auto_seeds, f)
+                shutil.copy(src, seeds_dir)
     else:
         print('No seeds dir found at: %s' % seeds_dir)
 if __name__ == '__main__':
