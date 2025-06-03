b6d3a90f749dcb4c13f8a995f03cd2de3acb0ceb
Mike Thompson <mfthomps@nps.edu>
2025-06-02 17:18:17 -0700

    open flags

---
+++ simics/monitorCore/openFlags.py
@@ -0,0 +1,23 @@
+O_ACCMODE = 0o00000003
+O_RDONLY = 0o00000000
+O_WRONLY = 0o00000001
+O_RDWR = 0o00000002
+O_CREAT = 0o00000100  
+O_TRUNC = 0o00001000
+
+def getFlags(flags):
+    retval = ''
+    if flags == 0:
+        retval = 'RDONLY'
+    else:
+        if flags & O_WRONLY > 0:
+            retval = 'WRONLY'
+        if flags & O_RDWR > 0:
+            retval = 'RDWR'
+        if flags & O_CREAT > 0:
+            retval = retval + ' CREAT'
+        if flags & O_TRUNC > 0:
+            retval = retval + ' TRUNC'
+    if len(retval) == 0:
+        retval = '0o%o' % flags
+    return retval
