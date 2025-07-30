459057bc4f74a2c6525fa70223fd110e278a45bd
Mike Thompson <mfthomps@nps.edu>
2025-07-30 08:39:18 -0700

    ppc blr instruction

--- simics/monitorCore/runToReturn.py
+++ simics/monitorCore/runToReturn.py
@@ -81,8 +81,22 @@ class RunToReturn():
             #    self.top.stopAndGo(self.stepOne)
         
         elif not instruct[1].startswith('blt') and not instruct[1].startswith('ble'):
-            self.lgr.debug('RunToReturn callHap calls:%d rets:%d eip: 0x%x %s' % (self.call_count, self.ret_count, eip, instruct[1]))
             self.call_count = self.call_count+1
+            if instruct[1].startswith('bl '):
+                call_addr_s = instruct[1].split()[1]
+                call_addr = None
+                try:
+                    call_addr = int(call_addr_s, 16)
+                except:
+                    self.lgr.error('fix remove this no hex for %s addr %s ??' % (instruct[1], call_addr_s))
+                if call_addr is not None:
+                    call_fun = self.top.getFunName(call_addr)
+                    call_from_fun = self.top.getFunName(eip)
+                    if call_fun is None and call_from_fun is None:
+                        self.lgr.debug('RunToReturn eip: 0x%x call none from none, hack it down' % eip)
+                        self.call_count = self.call_count - 1
+                    else:
+                        self.lgr.debug('RunToReturn callHap calls now:%d rets:%d eip: 0x%x %s call_fun %s from %s' % (self.call_count, self.ret_count, eip, instruct[1], call_fun, call_from_fun))
 
     def retHap(self, dumb, context, break_num, memory):
         if self.ret_hap is None:
@@ -92,8 +106,14 @@ class RunToReturn():
             return
         sp = self.top.getReg('sp', self.cpu)
         eip = self.top.getEIP()
-        self.lgr.debug('RunToReturn retHap calls:%d rets:%d  sp: 0x%x eip: 0x%x' % (self.call_count, self.ret_count, sp, eip))
+        instruct = self.top.disassembleAddress(self.cpu, eip)
         self.ret_count = self.ret_count+1
+        if instruct[1].startswith('blr'):
+            lr = self.top.getReg('lr', self.cpu)
+            lr_fun = self.top.getFunName(lr)
+            call_from_fun = self.top.getFunName(eip)
+            self.lgr.debug('RunToReturn retHap calls:%d rets now:%d  sp: 0x%x eip: 0x%x %s lr 0x%x to fun %s from fun %s' % (self.call_count, self.ret_count, sp, eip, instruct[1], lr, lr_fun, call_from_fun))
+             
         if self.call_count == self.ret_count:
             SIM_run_alone(self.rmHaps, None)
             self.top.stopAndGo(self.stepOne)
