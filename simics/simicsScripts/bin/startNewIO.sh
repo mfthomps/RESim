4b0239a180144718cf34163e240d1c8c640b944f
Mike Thompson <mfthomps@nps.edu>
2025-11-13 18:33:57 -0800

    diagnostics

--- simics/simicsScripts/bin/startNewIO.sh
+++ simics/simicsScripts/bin/startNewIO.sh
@@ -12,7 +12,7 @@ target_prog=$3
 target_fd=$4
 here=$(pwd)
 target=$(basename $here)
-/home/mike/newio/ppc32/next_level/file.io/next_level/file.io.15if [ "$#" -lt 4 ]; then
+if [ "$#" -lt 4 ]; then
     echo "startNewIO.sh ini first_io target_prog target_fd"
     exit 1
 fi
@@ -24,13 +24,18 @@ target_dir="$top_dir"/"$target"
 mkdir -p $target_dir
 mkdir -p "$target_dir"/queue
 wm_dir="$target_dir"/trackio
+trace_dir="$target_dir"/traces
 mkdir -p $wm_dir
+mkdir -p $trace_dir
 queue_dir="$target_dir"/queue/
 cp $first_io $queue_dir
 #no_ext="${first_io%.*}" 
 wm_file="$wm_dir"/"$first_io"
+trace_file="$trace_dir"/"$first_io"
 echo "<><><><><>startNewIOi.sh Injecting $first_io"
 injectTarget.sh $ini $first_io $target_prog $target_fd $wm_file
+echo "<><><><><>startNewIOi.sh Injecting trace $first_io"
+injectTrace.sh $ini $first_io $trace_file
 echo "<><><><><><>startNewIO.sh Playing $first_io"
 playTarget.sh $ini $queue_dir $target_prog $target_fd
 next_level_dir="$target_dir"/next_level
