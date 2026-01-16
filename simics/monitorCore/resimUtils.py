cda8e854d440be35ce8a427173d4b691b4d37fa5
Mike Thompson <mfthomps@nps.edu>
2026-01-16 11:01:26 -0800

    add bit array operations

--- simics/monitorCore/resimUtils.py
+++ simics/monitorCore/resimUtils.py
@@ -857,4 +857,43 @@ def getTokenValue(line, token):
             tparts = remain.split(' ')
             retval = tparts[0].strip()
     return retval
+# A bit array demo - written for Python 3.0
+# From demo on python.org
+import array
+def makeBitArray(bitSize, fill = 0):
+    intSize = bitSize >> 5                   # number of 32 bit integers
+    if (bitSize & 31):                      # if bitSize != (32 * n) add
+        intSize += 1                        #    a record for stragglers
+    if fill == 1:
+        fill = 4294967295                                 # all bits set
+    else:
+        fill = 0                                      # all bits cleared
 
+    bitArray = array.array('I')          # 'I' = unsigned 32-bit integer
+
+    bitArray.extend((fill,) * intSize)
+
+    return(bitArray)
+
+# testBit() returns a nonzero result, 2**offset, if the bit at 'bit_num' is set to 1.
+def testBit(array_name, bit_num):
+    record = bit_num >> 5
+    offset = bit_num & 31
+    mask = 1 << offset
+    return(array_name[record] & mask)
+
+# setBit() returns an integer with the bit at 'bit_num' set to 1.
+def setBit(array_name, bit_num):
+    record = bit_num >> 5
+    offset = bit_num & 31
+    mask = 1 << offset
+    array_name[record] |= mask
+    return(array_name[record])
+
+# clearBit() returns an integer with the bit at 'bit_num' cleared.
+def clearBit(array_name, bit_num):
+    record = bit_num >> 5
+    offset = bit_num & 31
+    mask = ~(1 << offset)
+    array_name[record] &= mask
+    return(array_name[record])
