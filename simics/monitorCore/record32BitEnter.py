9da03aead1c3514a0bc33461faece3cbe56e7a2a
Mike Thompson <mfthomps@nps.edu>
2026-07-16 13:25:20 -0700

    separate steps for int 128 and sysenter

--- simics/monitorCore/record32BitEnter.py
+++ simics/monitorCore/record32BitEnter.py
@@ -31,56 +31,109 @@ import pickle
 from simics import *
 from resimHaps import *
 class Record32BitEnter():
-    def __init__(self, top, cpu, target, current_param, lgr, save=True):
+    def __init__(self, top, cpu, target, current_param, want, lgr, save=True):
         self.top = top
         self.cpu = cpu
         self.target = target
         self.lgr = lgr
         self.save = save
+        self.want = want
         # as originally constituted this is what we want to overwrite with new enter values
         fname = '%s.param' % self.target
         self.param = pickle.load(open( fname, "rb" ))
 
         self.enter_delta = current_param.sysenter - self.param.sysenter 
         self.lgr.debug('record32BitEnter enter_delta (current_param.sysenter (0x%x) - original.sysenter (0x%x) is 0x%x' % (current_param.sysenter, self.param.sysenter, self.enter_delta))
-        self.mode_hap = RES_hap_add_callback_obj("Core_Mode_Change", cpu, 0, self.modeHap, None)
         # set to none for testing 
-        if current_param.sys_entry is not None:
-            self.lgr.debug('original sys_entry was 0x%x, current 0x%x' % (self.param.sys_entry, current_param.sys_entry))
-            self.lgr.debug('original sysenter_32 was 0x%x, current 0x%x' % (self.param.sysenter_32, current_param.sysenter_32))
-        self.param.sys_entry = None
-        self.param.sysenter_32 = None
+        if current_param.sys_entry is not None and self.param.sys_entry is not None and want == 'int 128':
+            self.param.sys_entry = None
+            self.lgr.debug('record32BitEnter original sys_entry was 0x%x, current 0x%x' % (self.param.sys_entry, current_param.sys_entry))
+        if current_param.sysenter_32 is not None and self.param.sysenter_32 is not None and want == 'sysenter':
+            self.param.sysenter_32 = None
+            self.lgr.debug('record32BitEnter original sysenter_32 was 0x%x, current 0x%x' % (self.param.sysenter_32, current_param.sysenter_32))
+        cpu, comm, tid =  self.top.getCurrentProc()
+        if comm.startswith('('):
+            # weird in kernel comm
+            comm = comm[1:-1]
+        self.comm = comm
+        #self.doSysenter()
+        self.lgr.debug('record32BitEnter set mode hap, want %s set self.comm to %s' % (self.want, self.comm))
+        self.mode_hap = RES_hap_add_callback_obj("Core_Mode_Change", self.cpu, 0, self.modeHap, self.want)
+        SIM_continue(0)
 
-    def modeHap(self, dumb, the_obj, old, new):
-        eip = self.top.getEIP()
+    #def doSysenter(self):
+    #    self.lgr.debug('record32BitEnter doSysenter')
+    #    self.mode_hap = RES_hap_add_callback_obj("Core_Mode_Change", self.cpu, 0, self.modeHap, 'sysenter')
+    #    SIM_continue(0)
+
+    #def doEntry(self):
+    #    self.lgr.debug('record32BitEnter doEntry')
+    #    self.mode_hap = RES_hap_add_callback_obj("Core_Mode_Change", self.cpu, 0, self.modeHap, 'int 128')
+    #    SIM_continue(0)
+
+    def doSysexit(self):
+        self.lgr.debug('record32BitEnter doSyexit')
+        self.mode_hap = RES_hap_add_callback_obj("Core_Mode_Change", self.cpu, 0, self.modeHap, 'sysexit')
+        SIM_continue(0)
+
+    def rmHap(self, hap):
+        RES_hap_delete_callback_id("Core_Mode_Change", hap)
+
+    def rmMode(self):
+        if self.mode_hap is not None:
+            hap = self.mode_hap
+            self.mode_hap = None
+            SIM_run_alone(self.rmHap, hap)
+
+    def modeHap(self, want, the_obj, old, new):
+        if self.mode_hap is None:
+            return
+        eip = self.top.getReg('rip')
         instruct = SIM_disassemble_address(self.cpu, eip, 1, 0)
         cpu, comm, tid =  self.top.getCurrentProc()
+        self.lgr.debug('record32BitEnter modeHap new %d eip 0x%x instruct %s tid:%s (%s) self.comm %s want %s' % (new, eip, instruct[1], tid, comm, self.comm, want))
         if new == Sim_CPU_Mode_Supervisor:
-            self.lgr.debug('record32BitEnter modeHap entering kernel eip 0x%x instruct %s tid:%s (%s)' % (eip, instruct[1], tid, comm))
-            if instruct[1].startswith('int 128') and self.param.sys_entry is None:
-                SIM_run_alone(self.top.stopAndCall, self.recordInt128)
-            elif instruct[1] == 'sysenter' and self.param.sysenter_32 is None:
-                SIM_run_alone(self.top.stopAndCall, self.recordEnter32)
+            if instruct[1].startswith(want): 
+                # is the syscall we want
+                self.lgr.debug('record32BitEnter modeHap entering kernel eip 0x%x instruct %s tid:%s (%s)' % (eip, instruct[1], tid, comm))
+                self.rmMode()
+                if want == 'sysenter':
+                    SIM_run_alone(self.top.stopAndCall, self.recordEnter32)
+                else:
+                    SIM_run_alone(self.top.stopAndCall, self.recordInt128)
         else:
-            self.lgr.debug('record32BitEnter modeHap returning to user eip 0x%x instruct %s tid:%s (%s)'% (eip, instruct[1], tid, comm))
+            self.lgr.debug('record32BitEnter returning to user comm <%s> self.commm <%s>  want: <%s>' % (comm, self.comm, want))
+            if want == 'sysexit' and comm == self.comm:
+                self.rmMode()
+                self.lgr.debug('record32BitEnter modeHap returning to user eip 0x%x instruct %s tid:%s (%s)'% (eip, instruct[1], tid, comm))
+                self.top.stopAndGo(self.recordExit32Alone)
 
     def recordInt128(self, dumb):
         eip = self.top.getEIP()
-        self.param.sys_entry = self.top.getEIP() - self.enter_delta
+        self.param.sys_entry = self.top.getEIP() + self.enter_delta
         self.lgr.debug('record32BitEnter recordInt128 eip: 0x%x set sys_entry to 0x%x enter_delta was 0x%x' % (eip, self.param.sys_entry, self.enter_delta))
-        if self.param.sysenter_32 is None:
-            SIM_continue(0)
-        else:
-            self.saveParams()
+        self.saveParams()
 
     def recordEnter32(self, dumb):
         eip = self.top.getEIP()
-        self.param.sysenter_32 = eip - self.enter_delta
+        self.param.sysenter_32 = eip + self.enter_delta
         self.lgr.debug('record32BitEnter recordEnter32 eip: 0x%x set sysenter_32 to 0x%x enter_delta was 0x%x' % (eip, self.param.sysenter_32, self.enter_delta))
-        if self.param.sys_entry is None:
-            SIM_continue(0)
-        else:
-            self.saveParams()
+        # we need to run to user return and then back up 1 to get the instruction and address
+        self.top.allowReverse()
+        self.doSysexit()
+
+    def recordExit32Alone(self, dumb=None):
+        cycle = self.cpu.cycles
+        self.lgr.debug('record32BitEnter recordExit32Alone cycles: 0x%x' % cycle)
+        prior = cycle - 1
+        self.top.skipToCycle(prior)
+        eip = self.top.getEIP()
+        self.param.sysexit = eip - self.enter_delta
+        self.lgr.debug('recorded sysexit as 0x%x' % self.param.sysexit)
+        self.saveParams()
+
+    def recordExit32(self, dumb):
+        SIM_run_alone(self.recordExit32Alone, None)
 
     def saveParams(self):
         if self.save:
