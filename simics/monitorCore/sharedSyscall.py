d4c92a58e1e38bc797b01a7bd3eb84cd5eade8cb
Mike Thompson <mfthomps@nps.edu>
2026-07-23 08:39:25 -0700

    remove debug

--- simics/monitorCore/sharedSyscall.py
+++ simics/monitorCore/sharedSyscall.py
@@ -1753,7 +1753,7 @@ class SharedSyscall():
 
     def foolSelect(self, fd):
         ''' Modify return values from select to reflect no data for this fd ''' 
-        self.lgr.debug('sharedSyscall foolSelect set select fd to %d' % fd)
+        #self.lgr.debug('sharedSyscall foolSelect set select fd to %d' % fd)
         self.fool_select = fd
 
     def modifySelect(self, select_info, eax):
